2d24478d0630acf33d62a42fffd09f553cbfbcb9
seyoungjoy <diddpwl80@gmail.com>
2023-02-07 00:54:53 +0900

    init commit

---
+++ prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 80,
+  trailingComma: "none",
+  tabWidth: 2,
+  semi: true,
+  bracketSpacing: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  endOfLine: "lf",
+  proseWrap: "preserve",
+  quoteProps: "as-needed",
+  useTabs: false,
+};
