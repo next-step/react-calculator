643171ecf4a99fb8c855b37f0edfe0bdba83f0c5
minhyeok0328 <alsgur0008@naver.com>
2023-02-08 02:23:35 +0900

    feat: package.json 에 lint scripts 추가

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,9 @@ import './index.css';
 import CalculatorProvider from '@/store/CalculatorProvider';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode>
-        <CalculatorProvider>
-            <App/>
-        </CalculatorProvider>
-    </React.StrictMode>,
+	<React.StrictMode>
+		<CalculatorProvider>
+			<App/>
+		</CalculatorProvider>
+	</React.StrictMode>,
 );
