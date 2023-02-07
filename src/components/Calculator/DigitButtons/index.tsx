e67fd09e81b5d3e26b8452f94e6bc77de7d91f7b
ag502 <ag502@naver.com>
2023-02-07 22:39:15 +0900

    fix: DigitButtons 숫자 0 추가 되게끔 배열 길이 확장

--- src/components/Calculator/DigitButtons/index.tsx
+++ src/components/Calculator/DigitButtons/index.tsx
@@ -3,7 +3,7 @@ import styles from "./index.module.css";
 export default function DigitButtons() {
   return (
     <div className={`${styles.digits} flex`}>
-      {Array.from({ length: 9 }, (_, idx) => 9 - idx).map((digit) => (
+      {Array.from({ length: 10 }, (_, idx) => 9 - idx).map((digit) => (
         <button className={styles.digit} key={digit}>
           {digit}
         </button>
