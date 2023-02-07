723709b82102822d0930632e8d1d10bc40df5a0f
minhyeok0328 <alsgur0008@naver.com>
2023-02-07 21:36:22 +0900

    fix: store 변수에 TS4023 오류나는 부분 수정

--- src/feature/calculator/calculatorSlice.ts
+++ src/feature/calculator/calculatorSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { MESSAGE } from '@/constants';
 
-interface CalculatorState {
+export interface CalculatorState {
 	value: string;
 }
 
