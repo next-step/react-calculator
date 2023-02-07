b36afd914a241bc9d81e7462cabf6002c30b9992
ag502 <ag502@naver.com>
2023-02-07 22:15:58 +0900

    remove: DigitButton 컴포넌트 제거

--- src/components/Calculator/DigitButtons/index.tsx
+++ src/components/Calculator/DigitButtons/index.tsx
@@ -1,11 +1,12 @@
-import DigitButton from "../DigitButton";
 import styles from "./index.module.css";
 
 export default function DigitButtons() {
   return (
     <div className={`${styles.digits} flex`}>
-      {Array.from({ length: 9 }, (_, idx) => 9 - idx).map((value) => (
-        <DigitButton key={value}>{value}</DigitButton>
+      {Array.from({ length: 9 }, (_, idx) => 9 - idx).map((digit) => (
+        <button className={styles.digit} key={digit}>
+          {digit}
+        </button>
       ))}
     </div>
   );
