75610c452e708019594f75d3ec1e32ecb1861c89
Dongmin Ahn <eamon3481@naver.com>
2023-02-07 20:54:25 +0900

    feat : add equal calculate util and constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,4 +3,14 @@ export const operations = {
   SUBTRACT: '-',
   MULTIPLY: 'X',
   DIVIDE: '/',
-} as const;
+  EQUAL: '=',
+} satisfies Record<typeof OPERATIONS_LIST[number], '+' | '-' | 'X' | '/' | '='>;
+
+export const DIGITS = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0] as const;
+export const OPERATIONS_LIST = [
+  'ADD',
+  'SUBTRACT',
+  'MULTIPLY',
+  'DIVIDE',
+  'EQUAL',
+] as const;
