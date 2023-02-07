843ec5396f63afb309862f8e2a27d61f6f3f3f6f
kimseongchan-kr <fpel8800@gmail.com>
2023-02-08 00:18:02 +0900

    chore: prettier & eslint 설정

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {},
+};
