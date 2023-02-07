9d1efbf86e289ab06f2edca65b31100a8642582d
Taeyoung Jang <xoxodudwkd@gmail.com>
2023-02-07 22:41:06 +0900

    test: 사칙연산에 대한 테스트 코드 작성
    __test__/calculate.test.tsx
    - react-testing-library를 이용하여 덧셈, 뺄셈, 곱셈 그리고 나눗셈
      에 대한 테스트 코드 작성

---
+++ src/__tests__/calculate.test.tsx
@@ -0,0 +1,76 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import App from '../App';
+
+test('2개의 숫자에 대해 덧셈이 가능하다.', () => {
+  render(<App />);
+
+  const total = screen.getByRole('heading', { level: 1 });
+
+  userEvent.click(screen.getByRole('button', { name: '7' }));
+  expect(total).toHaveTextContent('7');
+
+  userEvent.click(screen.getByRole('button', { name: '+' }));
+  expect(total).toHaveTextContent('7+');
+
+  userEvent.click(screen.getByRole('button', { name: '4' }));
+  expect(total).toHaveTextContent('7+4');
+
+  userEvent.click(screen.getByRole('button', { name: '=' }));
+  expect(total).toHaveTextContent('11');
+});
+
+test('2개의 숫자에 대해 뺄셈이 가능하다.', () => {
+  render(<App />);
+
+  const total = screen.getByRole('heading', { level: 1 });
+
+  userEvent.click(screen.getByRole('button', { name: '7' }));
+  expect(total).toHaveTextContent('7');
+
+  userEvent.click(screen.getByRole('button', { name: '-' }));
+  expect(total).toHaveTextContent('7-');
+
+  userEvent.click(screen.getByRole('button', { name: '4' }));
+  expect(total).toHaveTextContent('7-4');
+
+  userEvent.click(screen.getByRole('button', { name: '=' }));
+  expect(total).toHaveTextContent('3');
+});
+
+test('2개의 숫자에 대해 곱셉이 가능하다.', () => {
+  render(<App />);
+
+  const total = screen.getByRole('heading', { level: 1 });
+
+  userEvent.click(screen.getByRole('button', { name: '7' }));
+  expect(total).toHaveTextContent('7');
+
+  userEvent.click(screen.getByRole('button', { name: 'X' }));
+  expect(total).toHaveTextContent('7X');
+
+  userEvent.click(screen.getByRole('button', { name: '4' }));
+  expect(total).toHaveTextContent('7X4');
+
+  userEvent.click(screen.getByRole('button', { name: '=' }));
+  expect(total).toHaveTextContent('28');
+});
+
+test('2개의 숫자에 대해 나눗셈이 가능하다.', () => {
+  render(<App />);
+
+  const total = screen.getByRole('heading', { level: 1 });
+
+  userEvent.click(screen.getByRole('button', { name: '7' }));
+  expect(total).toHaveTextContent('7');
+
+  userEvent.click(screen.getByRole('button', { name: '/' }));
+  expect(total).toHaveTextContent('7/');
+
+  userEvent.click(screen.getByRole('button', { name: '4' }));
+  expect(total).toHaveTextContent('7/4');
+
+  userEvent.click(screen.getByRole('button', { name: '=' }));
+  expect(total).toHaveTextContent('1');
+});
