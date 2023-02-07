209adf2fba424967f568f1772e4da59400307c8b
minhyeok0328 <alsgur0008@naver.com>
2023-02-08 02:21:11 +0900

    refactor: 렌더링 최적화

--- src/hooks/useCalculator.ts
+++ src/hooks/useCalculator.ts
@@ -2,19 +2,19 @@ import { useContext } from 'react';
 import { CalculatorContext } from '@/store/CalculatorContext';
 
 export default function useCalculator() {
-    const {
-        insertDigits,
-        insertOperation,
-        setAnswer,
-        resetCalculator,
-        state
-    } = useContext(CalculatorContext);
+	const {
+		insertDigits,
+		insertOperation,
+		setAnswer,
+		resetCalculator,
+		state
+	} = useContext(CalculatorContext);
 
-    return {
-        insertDigits,
-        insertOperation,
-        setAnswer,
-        resetCalculator,
-        state
-    };
+	return {
+		insertDigits,
+		insertOperation,
+		setAnswer,
+		resetCalculator,
+		state
+	};
 }
--- src/store/CalculatorProvider.tsx
+++ src/store/CalculatorProvider.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useReducer } from 'react';
+import { ReactNode, useCallback, useMemo, useReducer } from 'react';
 import { calculatorReducer, initialState } from '@/store/calculatorReducer';
 import Validator from '@/core/Validator';
 import { MESSAGE, OPERATOR } from '@/constants';
@@ -6,60 +6,61 @@ import Calculator from '@/domain/Calculator';
 import { CalculatorContext } from '@/store/CalculatorContext';
 
 export default function CalculatorProvider({ children }: { children: ReactNode }) {
-    const [state, dispatch] = useReducer(calculatorReducer, initialState);
+	const [state, dispatch] = useReducer(calculatorReducer, initialState);
+	const calculatorState = useMemo(() => state, [state]);
 
-    const insertDigits = (digits) => {
-        const updateDigits = state + digits;
+	const insertDigits = useCallback((digits: string) => {
+		const updateDigits = calculatorState + digits;
 
-        if (!Validator.isOverMaxDigitLength(updateDigits)) {
-            alert(MESSAGE.MAX_DIGIT_LENGTH);
-            return;
-        }
+		if (!Validator.isOverMaxDigitLength(updateDigits)) {
+			alert(MESSAGE.MAX_DIGIT_LENGTH);
+			return;
+		}
 
-        dispatch({ type: 'insertDigits', payload: digits });
-    };
+		dispatch({ type: 'insertDigits', payload: digits });
+	}, [calculatorState]);
 
-    const insertOperation = (operator) => {
-        if (state === '0') {
-            return;
-        }
+	const insertOperation = useCallback((operator: string) => {
+		if (calculatorState === '0') {
+			return;
+		}
 
-        if (Validator.isMaxOperatorLength(state + operator)) {
-            alert(MESSAGE.MAX_OPERATOR_LENGTH);
-            return;
-        }
+		if (Validator.isMaxOperatorLength(calculatorState + operator)) {
+			alert(MESSAGE.MAX_OPERATOR_LENGTH);
+			return;
+		}
 
-        dispatch({ type: 'insertOperation', payload: operator });
-    };
+		dispatch({ type: 'insertOperation', payload: operator });
+	}, [calculatorState]);
 
-    const setAnswer = () => {
-        const getOperator = state.split('').find((item) => OPERATOR.includes(item));
+	const setAnswer = useCallback(() => {
+		const getOperator = calculatorState.split('').find((item) => OPERATOR.includes(item));
 
-        if (!Validator.isOverMaxDigitLength(state) || !getOperator) {
-            return;
-        }
+		if (!Validator.isOverMaxDigitLength(calculatorState) || !getOperator) {
+			return;
+		}
 
-        const item = new Calculator(
-            getOperator,
-            state.split(getOperator).map((item) => Number(item))
-        );
+		const item = new Calculator(
+			getOperator,
+			calculatorState.split(getOperator).map((item) => Number(item))
+		);
 
-        dispatch({ type: 'setAnswer', payload: item.result() });
-    };
+		dispatch({ type: 'setAnswer', payload: item.result() });
+	}, [calculatorState]);
 
-    const resetCalculator = () => dispatch({ type: 'resetCalculator' });
+	const resetCalculator = () => dispatch({ type: 'resetCalculator' });
 
-    return (
-        <CalculatorContext.Provider
-            value={{
-                insertDigits,
-                insertOperation,
-                setAnswer,
-                resetCalculator,
-                state
-            }}
-        >
-            {children}
-        </CalculatorContext.Provider>
-    );
+	return (
+		<CalculatorContext.Provider
+			value={{
+				insertDigits,
+				insertOperation,
+				setAnswer,
+				resetCalculator,
+				state: calculatorState
+			}}
+		>
+			{children}
+		</CalculatorContext.Provider>
+	);
 }
