00b9eb3b9650b1bb4d34c0fca4ab14e6f8e0e766
waterhumanB <dpfflsk45@gmail.com>
2023-02-08 10:31:39 +0900

    chore: vite로 react 설치 및 기본 환경설정

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./css/index.css";
+
+function App() {
+  return (
+    <main>
+      <div>React Calculator</div>
+    </main>
+  );
+}
+
+export default App;
