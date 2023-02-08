697856a9db847e648092086f356f51af14531752
kimseongchan-kr <fpel8800@gmail.com>
2023-02-08 23:01:51 +0900

    feat(App, Calculator): 기존 html 파일을 Calculator 컴포넌트로 이동

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+import Calculator from './components/calculator/Calculator';
+
+const App = () => {
+  return (
+    <div id="app">
+      <Calculator />
+    </div>
+  );
+};
+
+export default App;
