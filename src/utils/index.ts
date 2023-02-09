2a5be9fc3eea7f0b5a0faad19d91206d3cd0ad41
Dongmin Ahn <eamon3481@naver.com>
2023-02-09 18:14:05 +0900

    refactor : resolve review comment

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,5 +3,5 @@ export const calculate = {
   SUBTRACT: (prev: number) => (next: number) => prev - next,
   MULTIPLY: (prev: number) => (next: number) => prev * next,
   DIVIDE: (prev: number) => (next: number) => prev / next,
-  EQUAL: (prev: number) => undefined,
+  EQUAL: (_: number) => null,
 };
