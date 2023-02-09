2c824666ce96155037060c4f8e661bed3d4a930b
jlee0505 <hj960322@gmail.com>
2023-02-09 16:22:31 +0900

    chore: console.log 삭제

--- react-calculator-app/src/components/Total.js
+++ react-calculator-app/src/components/Total.js
@@ -2,7 +2,6 @@ import '../css/total.css';
 import { useCalculator } from '../store/CalcContext';
 const Total = () => {
   const { ...state } = useCalculator();
-  console.log(state);
   return <h1 id="total">{state.total}</h1>;
 };
 
