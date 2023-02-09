626da0c9ef84956784632e48051a5be222189522
waterhumanB <dpfflsk45@gmail.com>
2023-02-09 18:02:56 +0900

    feat: total 컴포넌트에 무한대 값 이중 삼항연산자로 예외 처리

--- src/components/total/index.tsx
+++ src/components/total/index.tsx
@@ -1,7 +1,15 @@
 import { ITotalProps } from "../../types/calculate";
 
 function Total({ calculation }: ITotalProps) {
-  return <h1 id="total">{calculation === "" ? 0 : calculation}</h1>;
+  return (
+    <h1 id="total">
+      {calculation !== ""
+        ? Number(calculation) === Infinity
+          ? "ERROR"
+          : calculation
+        : 0}
+    </h1>
+  );
 }
 
 export default Total;
