9180d2f198cec3e0de90a7ac98ce8bf1c27674ca
minhyeok0328 <alsgur0008@naver.com>
2023-02-09 23:17:43 +0900

    fix: vite-plugin-eslint을 devDependencies로 이동

--- src/container/DigitsContainer.tsx
+++ src/container/DigitsContainer.tsx
@@ -4,13 +4,12 @@ interface Props {
 	insertDigits: (digits: string) => void;
 }
 
-const DIGIT = ['9', '8', '7', '6', '5', '4', '3', '2', '1', '0'];
+const DIGITS = ['9', '8', '7', '6', '5', '4', '3', '2', '1', '0'];
 
 export default function DigitsContainer({ insertDigits }: Props) {
-
 	return (
 		<div className="digits flex">
-			{DIGIT.map((digits) => (
+			{DIGITS.map((digits) => (
 				<Digits
 					key={digits}
 					onClick={insertDigits}
