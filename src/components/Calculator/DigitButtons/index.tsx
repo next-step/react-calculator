bd9d8ecb2389934c29d4da834ce41872a910dccc
ag502 <ag502@naver.com>
2023-02-09 22:05:33 +0900

    fix: 9 ~ 0 숫자 배열 상수로 분리

--- src/components/Calculator/DigitButtons/index.tsx
+++ src/components/Calculator/DigitButtons/index.tsx
@@ -4,10 +4,12 @@ interface DigitButtonsProps {
   onClickDigit(value: number): void;
 }
 
+const DIGITS = Array.from({ length: 10 }, (_, idx) => 9 - idx);
+
 export default function DigitButtons({ onClickDigit }: DigitButtonsProps) {
   return (
     <div className={`${styles.digits} flex`}>
-      {Array.from({ length: 10 }, (_, idx) => 9 - idx).map((digit) => (
+      {DIGITS.map((digit) => (
         <button
           className={styles.digit}
           key={digit}
