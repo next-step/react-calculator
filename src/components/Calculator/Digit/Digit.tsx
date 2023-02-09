5c37cc584143a28087ca9d232e5af4425a832709
퉁이리 <wer4272@nate.com>
2023-02-09 23:41:49 +0900

    fix: const 후 type
    Co-authored-by: InSeong-So <goflvhxj2547@gmail.com>

--- src/components/Calculator/Digit/Digit.tsx
+++ src/components/Calculator/Digit/Digit.tsx
@@ -18,27 +18,5 @@ const Digit = ({ appendDigit }: { appendDigit: (n: DigitNumbers) => void }) => {
 
 export default Digit;
 
-export type DigitNumbers =
-  | '9'
-  | '8'
-  | '7'
-  | '6'
-  | '5'
-  | '4'
-  | '3'
-  | '2'
-  | '1'
-  | '0';
-
-const DIGIT_NUMBERS: ReadonlyArray<DigitNumbers> = [
-  '9',
-  '8',
-  '7',
-  '6',
-  '5',
-  '4',
-  '3',
-  '2',
-  '1',
-  '0',
-];
+const DIGIT_NUMBERS = ['9', '8', '7', '6', '5', '4', '3', '2', '1', '0'] as const;
+type DigitNumbers = typeof DIGIT_NUMBERS[number];
