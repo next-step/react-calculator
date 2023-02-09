9eec78682d86be58ea8a9245492e38fade0f6828
tooooo1 <wer4272@nate.com>
2023-02-09 12:07:40 +0900

    refactor: index.tsx -> index.ts

--- src/components/Calculator/index.tsx
+++
@@ -1,5 +0,0 @@
-export { default as Digit } from '../Calculator/Digit';
-export { default as Modifier } from '../Calculator/Modifier';
-export { default as Operation } from '../Calculator/Operation';
-export { default as TotalScreen } from '../Calculator/TotalScreen';
-export { default } from './Calculator';
