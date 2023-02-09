e78a977c95d68232cc6160ec78942312ccf664d6
Taeyoung Jang <xoxodudwkd@gmail.com>
2023-02-09 23:02:39 +0900

    chore: 에러 메시지 상수 처리

--- src/constant.ts
+++ src/constant.ts
@@ -19,6 +19,11 @@ const OPERATION = {
   CALCULATE: '=',
 } as const;
 
+const ERROR_MESSAGE = {
+  INIFINITY: '오류',
+  MAX_LENGTH: '숫자는 세 자리까지만 입력 가능합니다!',
+  NUMBER_FIRST: '숫자를 먼저 입력한 후 연산자를 입력해주세요!',
+} as const;
 const MAX_LENGTH = 3;
 
-export { DIGIT, OPERATION, MAX_LENGTH };
+export { DIGIT, OPERATION, MAX_LENGTH, ERROR_MESSAGE };
