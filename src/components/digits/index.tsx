6162d82f9301a944626a93c7c10fbe959dbe4141
waterhumanB <dpfflsk45@gmail.com>
2023-02-10 14:53:35 +0900

    refactor: digits 컴포넌트 btn 태그 배열, map 정의로 간소화

--- src/components/digits/index.tsx
+++ src/components/digits/index.tsx
@@ -19,39 +19,13 @@ function Digits({ calculation, setCalculation }: IDigitsProps) {
       setDigits("");
     }
   }, [calculation]);
-
   return (
     <div className="digits flex">
-      <button onClick={digitHandler} className="digit">
-        9
-      </button>
-      <button onClick={digitHandler} className="digit">
-        8
-      </button>
-      <button onClick={digitHandler} className="digit">
-        7
-      </button>
-      <button onClick={digitHandler} className="digit">
-        6
-      </button>
-      <button onClick={digitHandler} className="digit">
-        5
-      </button>
-      <button onClick={digitHandler} className="digit">
-        4
-      </button>
-      <button onClick={digitHandler} className="digit">
-        3
-      </button>
-      <button onClick={digitHandler} className="digit">
-        2
-      </button>
-      <button onClick={digitHandler} className="digit">
-        1
-      </button>
-      <button onClick={digitHandler} className="digit">
-        0
-      </button>
+      {[...Array(10)].map((data, idx) => (
+        <button className="digit" onClick={digitHandler} key={idx}>
+          {idx}
+        </button>
+      ))}
     </div>
   );
 }
