10190eb0a1f9cd1611676d606e46baea5890f27b
kimseongchan-kr <fpel8800@gmail.com>
2023-02-10 14:22:40 +0900

    feat(Calculator, Button): handleTextInsert 함수 추가 및 버튼에 이벤트 			  적용

--- src/components/calculator/Calculator.jsx
+++ src/components/calculator/Calculator.jsx
@@ -1,31 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../common/Button';
 
 const Calculator = () => {
+  const [totalText, setTotalText] = useState('');
+
+  const handleTextInsert = (event) => {
+    const buttonValue = event.target.value;
+    setTotalText(totalText + buttonValue);
+  };
+
   return (
     <div className="calculator">
-      <h1 id="total">0</h1>
+      <h1 id="total">{totalText}</h1>
       <div className="digits flex">
-        <Button className="digit" value="9" />
-        <Button className="digit" value="8" />
-        <Button className="digit" value="7" />
-        <Button className="digit" value="6" />
-        <Button className="digit" value="5" />
-        <Button className="digit" value="4" />
-        <Button className="digit" value="3" />
-        <Button className="digit" value="2" />
-        <Button className="digit" value="1" />
-        <Button className="digit" value="0" />
+        <Button className="digit" value="9" onClick={handleTextInsert} />
+        <Button className="digit" value="8" onClick={handleTextInsert} />
+        <Button className="digit" value="7" onClick={handleTextInsert} />
+        <Button className="digit" value="6" onClick={handleTextInsert} />
+        <Button className="digit" value="5" onClick={handleTextInsert} />
+        <Button className="digit" value="4" onClick={handleTextInsert} />
+        <Button className="digit" value="3" onClick={handleTextInsert} />
+        <Button className="digit" value="2" onClick={handleTextInsert} />
+        <Button className="digit" value="1" onClick={handleTextInsert} />
+        <Button className="digit" value="0" onClick={handleTextInsert} />
       </div>
       <div className="modifiers subgrid">
-        <Button className="modifier" value="AC" />
+        <Button className="modifier" value="AC" onClick={handleTextInsert} />
       </div>
       <div className="operations subgrid">
-        <Button className="operation" value="/" />
-        <Button className="operation" value="X" />
-        <Button className="operation" value="-" />
-        <Button className="operation" value="+" />
-        <Button className="operation" value="=" />
+        <Button className="operation" value="/" onClick={handleTextInsert} />
+        <Button className="operation" value="X" onClick={handleTextInsert} />
+        <Button className="operation" value="-" onClick={handleTextInsert} />
+        <Button className="operation" value="+" onClick={handleTextInsert} />
+        <Button className="operation" value="=" onClick={handleTextInsert} />
       </div>
     </div>
   );
--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const Button = ({ className, value }) => {
-  return <button className={className}>{value}</button>;
+const Button = ({ className, value, onClick }) => {
+  return (
+    <button className={className} value={value} onClick={onClick}>
+      {value}
+    </button>
+  );
 };
 
 export default Button;
