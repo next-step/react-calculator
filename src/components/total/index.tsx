8db3f0ec1a0f84fa620e9429357b3d49f177165d
waterhumanB <dpfflsk45@gmail.com>
2023-02-10 15:57:08 +0900

    refactor: total 컴포넌트 렌더 함수 변수명 수정

--- src/components/total/index.tsx
+++ src/components/total/index.tsx
@@ -1,13 +1,13 @@
 import { ITotalProps } from "../../types/allProps";
 
 function Total({ calculation }: ITotalProps) {
-  const totalValue = (value: string) => {
+  const calcuatedValue = (value: string) => {
     if (value === "") {
       return "0";
     }
     return Number(value) === Infinity ? "ERROR" : value;
   };
-  return <h1 id="total">{totalValue(calculation)}</h1>;
+  return <h1 id="total">{calcuatedValue(calculation)}</h1>;
 }
 
 export default Total;
