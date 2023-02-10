4cf3dac87afcf20b16f1935cf88a18f954665c79
alwayshere <76519867+herekim@users.noreply.github.com>
2023-02-10 23:03:16 +0900

    chore: Add path alias

---
+++ craco.config.js
@@ -0,0 +1,10 @@
+const path = require('path')
+
+module.exports = {
+  webpack: {
+    alias: {
+      '@': path.resolve(__dirname, 'src/'),
+      '@css': path.resolve(__dirname, 'src/css'),
+    },
+  },
+}
