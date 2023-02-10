90fa72d7a00631ce9235d2fdb679611f1656ab85
kimseongchan-kr <fpel8800@gmail.com>
2023-02-11 03:07:44 +0900

    test(Calculator): "계산된 결과값을 가지고 추가적으로 계사을 진행할 수   있다. (사칙연산)" 테스트 코드 작성

--- src/components/calculator/Calculator.test.js
+++ src/components/calculator/Calculator.test.js
@@ -67,6 +67,36 @@ test('= 버튼을 클릭하면 식이 계산된다. (곱하기)', () => {
   expect(h1Element).toHaveTextContent('8');
 });
 
+test('계산된 결과값을 가지고 추가적으로 계산을 진행할 수 있다. (사칙연산)', () => {
+  render(<Calculator />);
+  const h1Element = screen.getByRole('heading');
+
+  fireEvent.click(screen.getByText('2'));
+  fireEvent.click(screen.getByText('+'));
+  fireEvent.click(screen.getByText('2'));
+  fireEvent.click(screen.getByText('='));
+
+  expect(h1Element).toHaveTextContent('4');
+
+  fireEvent.click(screen.getByText('-'));
+  fireEvent.click(screen.getByText('2'));
+  fireEvent.click(screen.getByText('='));
+
+  expect(h1Element).toHaveTextContent('2');
+
+  fireEvent.click(screen.getByText('X'));
+  fireEvent.click(screen.getByText('2'));
+  fireEvent.click(screen.getByText('='));
+
+  expect(h1Element).toHaveTextContent('4');
+
+  fireEvent.click(screen.getByText('/'));
+  fireEvent.click(screen.getByText('2'));
+  fireEvent.click(screen.getByText('='));
+
+  expect(h1Element).toHaveTextContent('2');
+});
+
 test('이상한 기호 또는 문자열이 들어갔을 때 오류 발생', () => {
   render(<Calculator />);
 
