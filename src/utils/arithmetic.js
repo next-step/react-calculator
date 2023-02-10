485bf9744ad843d72cbaef9d6d8fc0c59999786b
kimseongchan-kr <fpel8800@gmail.com>
2023-02-11 02:45:04 +0900

    feat(arithmetic): 잘못된 문자열이 식에 포함되어 있을 경우 에러 알림창이 		  뜨도록 작업

--- src/utils/arithmetic.js
+++ src/utils/arithmetic.js
@@ -8,5 +8,8 @@ export const arithmetic = (augend, addend, operation) => {
       return Math.floor(augend / addend);
     case 'X':
       return augend * addend;
+    default:
+      alert('잘못된 기호 또는 문자가 포함되어있습니다.');
+      return '';
   }
 };
