ff3d8878152bf2ff3889422721df5c6dafa66b6c
jlee0505 <hj960322@gmail.com>
2023-02-10 16:07:06 +0900

    chore: 불필요한 값 꺼내오기 제거

--- react-calculator-app/src/components/Total.js
+++ react-calculator-app/src/components/Total.js
@@ -1,8 +1,8 @@
 import '../css/total.css';
 import { useCalculator } from '../store/CalcContext';
 const Total = () => {
-  const { ...state } = useCalculator();
-  return <h1 id="total">{state.total}</h1>;
+  const { total } = useCalculator();
+  return <h1 id="total">{total}</h1>;
 };
 
 export default Total;
