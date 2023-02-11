430b843aff5e09f3427c46ee70137f32f67bf5ae
alwayshere <76519867+herekim@users.noreply.github.com>
2023-02-11 20:45:09 +0900

    modify: discard below decimal point

--- src/utils/calculator.ts
+++ src/utils/calculator.ts
@@ -2,5 +2,5 @@ export const calculator = {
   '+': (a: number, b: number) => a + b,
   '-': (a: number, b: number) => a - b,
   X: (a: number, b: number) => a * b,
-  '/': (a: number, b: number) => Number((a / b).toFixed(2)),
+  '/': (a: number, b: number) => Math.floor(a / b),
 }
