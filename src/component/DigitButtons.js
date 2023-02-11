91bf21dc13d5317c7590c2b0ea980cb21e445a08
realhee <wlsgml5628@gmail.com>
2023-02-12 00:32:07 +0900

    Refactor: 미사용 DigitButtons 파라미터 삭제

--- src/component/DigitButtons.js
+++ src/component/DigitButtons.js
@@ -1,7 +1,7 @@
 import { digits } from "../const";
 import Button from "./Button";
 
-export default function DigitButtons({ className, value }) {
+export default function DigitButtons() {
   const digitButtons = digits.map((v) => (
     <Button className="digit" key={v} value={v} />
   ));
