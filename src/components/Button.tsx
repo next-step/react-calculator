bfe2b5446fe9309bdca0d51d8e32ef1703d5b4f9
Taeyoung Jang <xoxodudwkd@gmail.com>
2023-02-13 02:33:54 +0900

    chore: 불필요한 타입 및 컴포넌트 삭제

--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import type { CombineElementProps } from 'types';
-
-type ButtonProps = CombineElementProps<'button'>;
-
-function Button({ children, ...props }: ButtonProps) {
-  return <button {...props}>{children}</button>;
-}
-
-export default Button;
