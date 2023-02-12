296821d36949f298619d8ded68449330fc25a358
gptplayer <gptplayer2023@gmail.com>
2023-02-13 00:55:11 +0900

    feat: init calculator project

---
+++ src/Calculator.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const Calculator = () => {
+  return (
+    <div id="app">
+      <div className="calculator">
+        <h1 id="total">0</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
