db0b8ec4aaa31bad8229d5251b06c4e8aef2ab69
kimseongchan-kr <fpel8800@gmail.com>
2023-02-12 10:13:55 +0900

    feat(error): 에러 메시지 상수화

---
+++ src/constants/error.js
@@ -0,0 +1,5 @@
+export const ERROR_TEXT = '오류';
+
+export const INPUT_NUMBER_FIRST_MESSAGE = '숫자를 먼저 입력한 후 연산자를 입력해주세요!';
+export const NUMBER_OF_EXCEPTION_MESSAGE = '숫자는 세 자리까지만 입력 가능합니다!';
+export const INPUT_NOT_ALLOWED_MESSAGE = '잘못된 기호 또는 문자가 포함되어있습니다.';
