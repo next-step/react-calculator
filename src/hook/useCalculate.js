fb6a0e0b5efaea65b65f26b27c03ab9196c1151d
seyoungjoy <diddpwl80@gmail.com>
2023-02-13 03:21:29 +0900

    refactor: 3자리 수 입력, 결과값 처리

--- src/hook/useCalculate.js
+++ src/hook/useCalculate.js
@@ -1,10 +1,11 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { calculate, splitTotal } from "../util";
 
 const useCalculate = () => {
   const [total, setTotal] = useState("0");
   const digitRef = useRef([]);
   const operatorRef = useRef(null);
+  const [isActive, setIsActive] = useState(true);
 
   const enterDigit = (digit) => {
     if (total === "0") {
@@ -20,13 +21,38 @@ const useCalculate = () => {
   };
 
   const calculateTotal = () => {
+    setIsActive(false);
     digitRef.current = splitTotal(total, operatorRef.current);
-    setTotal(calculate(...digitRef.current, operatorRef.current));
+    const result = calculate(...digitRef.current, operatorRef.current);
+    if (result === Infinity) setTotal("오류");
+    else setTotal(Math.floor(result));
   };
 
   const clear = () => {
     setTotal("0");
+    operatorRef.current = null;
+    setIsActive(true);
   };
+
+  const checkDigit = () => {
+    if (operatorRef.current) {
+      if (splitTotal(total, operatorRef.current)[1].length > 3) {
+        alert("숫자는 세 자리까지만 입력 가능합니다!");
+        setTotal(total.slice(0, total.length - 1));
+      }
+    } else {
+      if (total.length > 3) {
+        alert("숫자는 세 자리까지만 입력 가능합니다!");
+        setTotal(total.slice(0, total.length - 1));
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (isActive) {
+      checkDigit();
+    }
+  }, [total, isActive]);
 
   return {
     total,
