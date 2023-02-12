729c0116fc53c0dfe8d4588e8b6cffaff2dcef3b
Shin seongsu <tlstjdtn321@naver.com>
2023-02-12 21:37:25 +0900

    feat: 사칙연산 구현

---
+++ src/utils/Operand.ts
@@ -0,0 +1,17 @@
+const mapOperation: Record<string, (num1: string, num2: string) => number> = {
+  "+": (num1, num2) => Number(num1) + Number(num2),
+  "-": (num1, num2) => Number(num2) - Number(num1),
+  X: (num1, num2) => Number(num1) * Number(num2),
+  "/": (num1, num2) => Number(num2) / Number(num1),
+};
+
+const calculate = (state: {
+  targetNumber: string;
+  prevNumber: string;
+  operator: string;
+}) => {
+  const { targetNumber, prevNumber, operator } = state;
+  return Math.floor(mapOperation[operator](targetNumber, prevNumber));
+};
+
+export { calculate };
