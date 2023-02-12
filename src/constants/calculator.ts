626396ddd9bfc455364f1cab70b8842b36cd5e1d
Taeyoung Jang <xoxodudwkd@gmail.com>
2023-02-12 20:44:18 +0900

    feat: 도메인 관련 상수 분리

---
+++ src/constants/calculator.ts
@@ -0,0 +1,23 @@
+enum Digit {
+  NINE = '9',
+  EIGHT = '8',
+  SEVEN = '7',
+  SIX = '6',
+  FIVE = '5',
+  FOUR = '4',
+  THREE = '3',
+  TWO = '2',
+  ONE = '1',
+  ZERO = '0',
+}
+
+const OPERATION = {
+  DIVIDE: '/',
+  MULTIPLE: 'X',
+  MINUS: '-',
+  PLUS: '+',
+} as const;
+
+const CALCULATE = '=';
+
+export { Digit, OPERATION, CALCULATE };
