781d10e100234bada130c95b11f62d2436370935
Taeyoung Jang <xoxodudwkd@gmail.com>
2023-02-13 01:48:25 +0900

    mod: CalculatorModel 클래스를 함수 모음으로 변경
    CalculatorModel.ts
    - 계산기 관련 멤버 변수와 메서드를 가지고 있던 클래스에서
      각 메서드를 일반 함수로 분리한 뒤 이름 모아놓은 파일로
      변경

--- src/components/Calculator/CalculatorModel.ts
+++ src/components/Calculator/CalculatorModel.ts
@@ -10,81 +10,64 @@ const calculationOptions = {
   [OPERATION.DIVIDE]: (a: number, b: number) => Math.floor(a / b),
 };
 
-class CalculatorModel {
-  #operation: Operation | null;
-  #leftOperand: number;
-  #rightOperand: number | null;
-  #accumulator: string;
-
-  constructor() {
-    this.#operation = null;
-    this.#leftOperand = 0;
-    this.#rightOperand = null;
-    this.#accumulator = '';
-  }
-
-  get operation() {
-    return this.#operation;
-  }
-  get leftOperand() {
-    return this.#leftOperand;
-  }
-  get rightOperand() {
-    return this.#rightOperand;
-  }
-  get accumulator() {
-    return this.#accumulator;
-  }
-
-  updateAccumulator() {
-    const leftOperand = String(this.#leftOperand);
-    const operation = this.#operation ?? '';
-    const rightOperand = String(this.#rightOperand ?? '');
-
-    this.#accumulator = leftOperand + operation + rightOperand;
-  }
-
-  setOperand(digit: Digit) {
-    if (!this.operation) {
-      this.#leftOperand = Number(this.#leftOperand + digit);
-    } else {
-      const newRightOperand = this.#rightOperand ? this.#rightOperand + digit : digit;
-      this.#rightOperand = Number(newRightOperand);
-    }
-
-    this.updateAccumulator();
-  }
-
-  setOperation(newOperation: Operation | null) {
-    this.#operation = newOperation;
-
-    this.updateAccumulator();
-  }
-
-  calculate() {
-    if (!this.#operation) {
-      throw new Error('Missing Operation');
-    }
-
-    if (!this.#rightOperand) {
-      throw new Error('Missing Right Operand');
-    }
-
-    const calculationOption = calculationOptions[this.#operation];
-    const newLeftOperand = calculationOption(this.#leftOperand, this.#rightOperand);
-
-    this.#leftOperand = newLeftOperand;
-    this.#operation = null;
-    this.#rightOperand = null;
-    this.updateAccumulator();
-  }
-
-  allClear() {
-    this.#leftOperand = 0;
-    this.#operation = null;
-    this.#rightOperand = null;
-    this.#accumulator = '';
-  }
+interface ICalculator {
+  operation: Operation | null;
+  leftOperand: number;
+  rightOperand: number | null;
+  accumulator: string;
 }
 
-export default CalculatorModel;
+const updateAccumulator = (calcaultor: ICalculator) => {
+  const { leftOperand, rightOperand, operation } = calcaultor;
+
+  const newOperation = operation ?? '';
+  const newRightOperand = String(rightOperand ?? '');
+  const newAccumulator = leftOperand + newOperation + newRightOperand;
+
+  return {
+    ...calcaultor,
+    accumulator: newAccumulator,
+  };
+};
+
+const updateOperand = (calcaultor: ICalculator, digit: Digit) => {
+  const { leftOperand, rightOperand, operation } = calcaultor;
+
+  return {
+    ...calcaultor,
+    ...(!operation && { leftOperand: Number(leftOperand + digit) }),
+    ...(operation && { rightOperand: Number(rightOperand ? rightOperand + digit : digit) }),
+  };
+};
+
+const updateOperation = (calcaultor: ICalculator, newOperation: Operation | null) => {
+  return {
+    ...calcaultor,
+    operation: newOperation,
+  };
+};
+
+const calculate = (calcaultor: ICalculator) => {
+  const { leftOperand, rightOperand, operation } = calcaultor;
+
+  if (!operation) {
+    throw new Error('Missing Operation');
+  }
+
+  if (rightOperand === null) {
+    throw new Error('Missing Right Operand');
+  }
+
+  const calculationOption = calculationOptions[operation];
+  const newLeftOperand = calculationOption(leftOperand, rightOperand);
+
+  return {
+    ...calcaultor,
+    leftOperand: newLeftOperand,
+    operation: null,
+    rightOperand: null,
+  };
+};
+
+export { updateAccumulator, updateOperand, updateOperation, calculate };
+export type { ICalculator };
