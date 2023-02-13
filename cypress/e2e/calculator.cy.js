e9378640f65e58fbfa2f10e4ba47e586cd6f964f
gptplayer <gptplayer2023@gmail.com>
2023-02-13 16:19:58 +0900

    test: add tests in which the numbers are accumulated on the display

--- cypress/e2e/calculator.cy.js
+++ cypress/e2e/calculator.cy.js
@@ -11,4 +11,19 @@ describe("계산기", () => {
     cy.get(".digit").contains("1").click();
     cy.get("#total").should("have.text", "1");
   });
+
+  it("숫자 버튼을 여러 번 누르면 display에 숫자가 누적 표시", () => {
+    cy.get(".digit").contains("1").click();
+    cy.get(".digit").contains("2").click();
+    cy.get(".digit").contains("3").click();
+    cy.get("#total").should("have.text", "123");
+  });
+
+  it("숫자 버튼은 최대 3자리까지만 표시", () => {
+    cy.get(".digit").contains("1").click();
+    cy.get(".digit").contains("2").click();
+    cy.get(".digit").contains("3").click();
+    cy.get(".digit").contains("4").click();
+    cy.get("#total").should("have.text", "123");
+  });
 });
