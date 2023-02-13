5b0604df7431cbbc41d1908f005372231ac1e91d
kimseongchan-kr <fpel8800@gmail.com>
2023-02-14 00:07:50 +0900

    mod(Button): children을 받아 태그의 텍스트로 사용하도록 변경

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Button = ({ className, value, onClick }) => {
+const Button = ({ className, value, onClick, children }) => {
   return (
     <button className={className} value={value} onClick={onClick}>
-      {value}
+      {children}
     </button>
   );
 };
