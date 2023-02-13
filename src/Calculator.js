bf51060cf5e8b05d1a2f115a5fe9cbd6a03c2d63
gptplayer <gptplayer2023@gmail.com>
2023-02-13 16:06:32 +0900

    refactor: separate digit components

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,10 +1,37 @@
 import React, { useState } from "react";
 
 const Calculator = () => {
+  const [total, setTotal] = useState(0);
+
+  const Digits = ({ onClickDigit }) => (
+    <div className="digits flex">
+      {Array.from({ length: 10 }, (_, i) => (
+        <button className="digit" key={i} onClick={() => onClickDigit(i)}>
+          {i}
+        </button>
+      )).reverse()}
+    </div>
+  );
+
+  const onClickDigit = (i) => {
+    setTotal(i);
+  };
+
   return (
     <div id="app">
-      <div className="calculator">
-        <h1 id="total">0</h1>
+      <div class="calculator">
+        <h1 id="total">{total}</h1>
+        <Digits onClickDigit={onClickDigit} />
+        <div class="modifiers subgrid">
+          <button class="modifier">AC</button>
+        </div>
+        <div class="operations subgrid">
+          <button class="operation">/</button>
+          <button class="operation">X</button>
+          <button class="operation">-</button>
+          <button class="operation">+</button>
+          <button className="operation">=</button>
+        </div>
       </div>
     </div>
   );
