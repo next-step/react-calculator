a9e7f9f8562a12c7c22aa866d757f8d50bf65bb4
gptplayer <gptplayer2023@gmail.com>
2023-02-13 16:01:54 +0900

    test: add a test that displays numbers on the display when the number button is pressed

--- cypress/e2e/calculator.cy.js
+++ cypress/e2e/calculator.cy.js
@@ -6,4 +6,9 @@ describe("계산기", () => {
   it("최초 렌더링 시 display에는 0 표시", () => {
     cy.get("#total").should("have.text", "0");
   });
+
+  it("숫자 버튼을 누르면 display에 숫자가 표시", () => {
+    cy.get(".digit").contains("1").click();
+    cy.get("#total").should("have.text", "1");
+  });
 });
