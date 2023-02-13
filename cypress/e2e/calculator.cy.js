b9637f7d375d29d4d938a6333c74083dc20e4a73
gptplayer <gptplayer2023@gmail.com>
2023-02-13 16:31:55 +0900

    refactor: separate frequently used querySelectors into constants

--- cypress/e2e/calculator.cy.js
+++ cypress/e2e/calculator.cy.js
@@ -1,37 +1,41 @@
+const DIGIT_SELECTOR = ".digit";
+const MODIFIER_SELECTOR = ".modifier";
+const DISPLAY_SELECTOR = "#total";
+
 describe("계산기", () => {
   beforeEach("페이지 방문", () => {
     cy.visit("http://localhost:3000");
   });
 
   it("최초 렌더링 시 display에는 0 표시", () => {
-    cy.get("#total").should("have.text", "0");
+    cy.get(DISPLAY_SELECTOR).should("have.text", "0");
   });
 
   it("숫자 버튼을 누르면 display에 숫자가 표시", () => {
-    cy.get(".digit").contains("1").click();
-    cy.get("#total").should("have.text", "1");
+    cy.get(DIGIT_SELECTOR).contains("1").click();
+    cy.get(DISPLAY_SELECTOR).should("have.text", "1");
   });
 
   it("숫자 버튼을 여러 번 누르면 display에 숫자가 누적 표시", () => {
-    cy.get(".digit").contains("1").click();
-    cy.get(".digit").contains("2").click();
-    cy.get(".digit").contains("3").click();
-    cy.get("#total").should("have.text", "123");
+    cy.get(DIGIT_SELECTOR).contains("1").click();
+    cy.get(DIGIT_SELECTOR).contains("2").click();
+    cy.get(DIGIT_SELECTOR).contains("3").click();
+    cy.get(DISPLAY_SELECTOR).should("have.text", "123");
   });
 
   it("숫자 버튼은 최대 3자리까지만 표시", () => {
-    cy.get(".digit").contains("1").click();
-    cy.get(".digit").contains("2").click();
-    cy.get(".digit").contains("3").click();
-    cy.get(".digit").contains("4").click();
-    cy.get("#total").should("have.text", "123");
+    cy.get(DIGIT_SELECTOR).contains("1").click();
+    cy.get(DIGIT_SELECTOR).contains("2").click();
+    cy.get(DIGIT_SELECTOR).contains("3").click();
+    cy.get(DIGIT_SELECTOR).contains("4").click();
+    cy.get(DISPLAY_SELECTOR).should("have.text", "123");
   });
 
   it("AC 버튼을 누르면 display에 0 표시", () => {
-    cy.get(".digit").contains("1").click();
-    cy.get(".digit").contains("2").click();
-    cy.get(".digit").contains("3").click();
-    cy.get(".modifier").click();
-    cy.get("#total").should("have.text", "0");
+    cy.get(DIGIT_SELECTOR).contains("1").click();
+    cy.get(DIGIT_SELECTOR).contains("2").click();
+    cy.get(DIGIT_SELECTOR).contains("3").click();
+    cy.get(MODIFIER_SELECTOR).click();
+    cy.get(DISPLAY_SELECTOR).should("have.text", "0");
   });
 });
