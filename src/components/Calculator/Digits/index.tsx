fb77a8e4a8fc364877eff66b50ad02aed0ebe932
Yujeong Choung <yujeong.choung@gmail.com>
2023-02-13 21:18:31 +0900

    feat: 자리수 3의 상수화

--- src/components/Calculator/Digits/index.tsx
+++ src/components/Calculator/Digits/index.tsx
@@ -1,16 +1,20 @@
-import React from "react";
-import { useCalculator } from "../../../modules/context/Calculator/CalculatorContext";
-import Digit from "./Digit";
+import React from 'react';
+import { useCalculator } from '../../../modules/context/Calculator/CalculatorContext';
+import Digit from './Digit';
 
 const digitList = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+const MAX_DIGIT_LENGTH = 3;
 
 const Digits = () => {
   const { calculator, addFirstDigit, addSecondDigit } = useCalculator();
 
   const handleDigitClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    if (calculator.firstDigits.length < 3 && !calculator.operation) {
+    if (
+      calculator.firstDigits.length < MAX_DIGIT_LENGTH &&
+      !calculator.operation
+    ) {
       addFirstDigit(e.currentTarget.value);
-    } else if (calculator.secondDigits.length < 3) {
+    } else if (calculator.secondDigits.length < MAX_DIGIT_LENGTH) {
       addSecondDigit(e.currentTarget.value);
     }
   };
