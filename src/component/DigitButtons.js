d529b054805f01f4688afaf84995918f91602f54
realhee <wlsgml5628@gmail.com>
2023-02-14 02:15:42 +0900

    Refactor: DigitButtons의 map 함수 매개변수 명시적으로 변경

--- src/component/DigitButtons.js
+++ src/component/DigitButtons.js
@@ -2,8 +2,8 @@ import { DIGIT_NUMBERS } from "../const";
 import Button from "./Button";
 
 export default function DigitButtons({ onClick }) {
-  const digits = DIGIT_NUMBERS.map((v) => (
-    <Button className="digit" key={v} value={v} onClick={onClick} />
+  const digits = DIGIT_NUMBERS.map((digit) => (
+    <Button className="digit" key={digit} value={digit} onClick={onClick} />
   ));
   return <div className="digits flex">{digits}</div>;
 }
