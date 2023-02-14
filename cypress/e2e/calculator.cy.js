32b90cd4bc17476b4aa1b986c5027702ff4ebc59
gptplayer <gptplayer2023@gmail.com>
2023-02-14 11:57:46 +0900

    refactor: seperate frequently used functions from test code into reusable functions

--- cypress/e2e/calculator.cy.js
+++ cypress/e2e/calculator.cy.js
@@ -2,49 +2,59 @@ const DIGIT_SELECTOR = ".digit";
 const MODIFIER_SELECTOR = ".modifier";
 const DISPLAY_SELECTOR = "#total";
 const OPERATION_SELECTOR = ".operation";
+const BASE_URL = "http://localhost:3000";
+
+const pressDigits = (digits) => {
+  digits.forEach((digit) => {
+    cy.get(DIGIT_SELECTOR).contains(digit).click();
+  });
+};
+
+const pressModifier = () => {
+  cy.get(MODIFIER_SELECTOR).click();
+};
+
+const pressOperation = (operation) => {
+  cy.get(OPERATION_SELECTOR).contains(operation).click();
+};
+
+const checkDisplay = (displayText) => {
+  cy.get(DISPLAY_SELECTOR).should("have.text", displayText);
+};
 
 describe("계산기", () => {
   beforeEach("페이지 방문", () => {
-    cy.visit("http://localhost:3000");
+    cy.visit(BASE_URL);
   });
 
   it("최초 렌더링 시 display에는 0 표시", () => {
-    cy.get(DISPLAY_SELECTOR).should("have.text", "0");
+    checkDisplay("0");
   });
 
   it("숫자 버튼을 누르면 display에 숫자가 표시", () => {
-    cy.get(DIGIT_SELECTOR).contains("1").click();
-    cy.get(DISPLAY_SELECTOR).should("have.text", "1");
+    pressDigits(["1"]);
+    checkDisplay("1");
   });
 
   it("숫자 버튼을 여러 번 누르면 display에 숫자가 누적 표시", () => {
-    cy.get(DIGIT_SELECTOR).contains("1").click();
-    cy.get(DIGIT_SELECTOR).contains("2").click();
-    cy.get(DIGIT_SELECTOR).contains("3").click();
-    cy.get(DISPLAY_SELECTOR).should("have.text", "123");
+    pressDigits(["1", "2", "3"]);
+    checkDisplay("123");
   });
 
   it("숫자 버튼은 최대 3자리까지만 표시", () => {
-    cy.get(DIGIT_SELECTOR).contains("1").click();
-    cy.get(DIGIT_SELECTOR).contains("2").click();
-    cy.get(DIGIT_SELECTOR).contains("3").click();
-    cy.get(DIGIT_SELECTOR).contains("4").click();
-    cy.get(DISPLAY_SELECTOR).should("have.text", "123");
+    pressDigits(["1", "2", "3", "4"]);
+    checkDisplay("123");
   });
 
   it("AC 버튼을 누르면 display에 0 표시", () => {
-    cy.get(DIGIT_SELECTOR).contains("1").click();
-    cy.get(DIGIT_SELECTOR).contains("2").click();
-    cy.get(DIGIT_SELECTOR).contains("3").click();
-    cy.get(MODIFIER_SELECTOR).click();
-    cy.get(DISPLAY_SELECTOR).should("have.text", "0");
+    pressDigits(["1", "2", "3"]);
+    pressModifier();
+    checkDisplay("0");
   });
 
   it("숫자 버튼을 누르고 연산자 버튼을 누르면 display에 연산자 표시", () => {
-    cy.get(DIGIT_SELECTOR).contains("1").click();
-    cy.get(DIGIT_SELECTOR).contains("2").click();
-    cy.get(DIGIT_SELECTOR).contains("3").click();
-    cy.get(OPERATION_SELECTOR).contains("+").click();
-    cy.get(DISPLAY_SELECTOR).should("have.text", "123+");
+    pressDigits(["1", "2", "3"]);
+    pressOperation("+");
+    checkDisplay("123+");
   });
 });
