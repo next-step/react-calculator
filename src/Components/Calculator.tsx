013270a02dbe1514d7229f71d3cc77a2c304c259
이국신 <nw4611@naver.com>
2023-02-26 23:06:49 +0900

    feat: 숫자를 3자리까지만 입력하도록 validation 추가

--- src/Components/Calculator.tsx
+++ src/Components/Calculator.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { isNumeric } from "../Utils/Numeric";
 import DigitBoard from "./DigitBoard"
 import ModifierButton from "./ModifierButton"
 import OperationBoard from "./OperationBoard";
@@ -7,7 +8,30 @@ import Total from "./Total"
 function Calculator() {
   const [expression, setExpression] = useState("0");
 
+
+  const canAddDigit = () => {
+    const lastIndex = expression.length - 1;
+    
+    if (lastIndex <= 1) {
+      return true;
+    }
+
+    // 숫자가 입력됐을 때 마지막 3글자가 숫자인지 판별
+    for (let i = 0; i < 3; i++) {
+      if (lastIndex - i >= 0 && !isNumeric(expression[lastIndex - i])) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
   const setDigit = (number: string) => {
+    if (!canAddDigit()) {
+      alert("숫자는 3자리까지만 입력 가능합니다!");
+      return;
+    }
+
     setExpression((prev) => (prev === "0" ? number : prev + number));
   }
 
---
+++ src/Utils/Numeric.ts
@@ -0,0 +1,1 @@
+export const isNumeric = (number: string) => !isNaN(+number);
