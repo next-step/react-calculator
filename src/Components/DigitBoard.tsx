fa5d591048b32b913a4b9bfd88b5ae960abc73aa
이국신 <nw4611@naver.com>
2023-03-02 15:22:15 +0900

    fix: 빠진 숫자 0을 추가

--- src/Components/DigitBoard.tsx
+++ src/Components/DigitBoard.tsx
@@ -1,6 +1,6 @@
 import DigitButton from './DigitButton';
 
-const digits = ["9", "8", "7", "6", "5", "4", "3", "2", "1"] as const;
+const digits = ["9", "8", "7", "6", "5", "4", "3", "2", "1", "0"] as const;
 
 interface DigitProps {
   setDigit(number: string): void;
