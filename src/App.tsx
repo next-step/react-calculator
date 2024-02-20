72a0ed9402ee294c4065f6e8a076a9d7956e5c2a
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-02-20 16:43:24 +0900

    index.html 파일에 있던 구조를 App.tsx로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+  return (  
+    <div id="app">
+      <div className="calculator">
+        <h1 id="total">0</h1>
+        <div className="digits flex">
+          <button className="digit">9</button>
+          <button className="digit">8</button>
+          <button className="digit">7</button>
+          <button className="digit">6</button>
+          <button className="digit">5</button>
+          <button className="digit">4</button>
+          <button className="digit">3</button>
+          <button className="digit">2</button>
+          <button className="digit">1</button>
+          <button className="digit">0</button>
+        </div>
+        <div className="modifiers subgrid">
+          <button className="modifier">AC</button>
+        </div>
+        <div className="operations subgrid">
+          <button className="operation">/</button>
+          <button className="operation">X</button>
+          <button className="operation">-</button>
+          <button className="operation">+</button>
+          <button className="operation">=</button>
+        </div>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
