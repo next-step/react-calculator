df0d438a8d9c247d692a8d73ff1daa6b1ae0632c
lomen-SeoJaeWan <sjw7324@lomen.kr>
2024-02-20 22:09:27 +0900

    init: jest

---
+++ src/utils/calculator.js
@@ -0,0 +1,19 @@
+class Calculator {
+  sum(a, b) {
+    return a + b;
+  }
+
+  sub(a, b) {
+    return a - b;
+  }
+
+  mul(a, b) {
+    return a * b;
+  }
+
+  div(a, b) {
+    return a / b;
+  }
+}
+
+export default Calculator;
---
+++ src/utils/calculator.test.js
@@ -0,0 +1,22 @@
+import { it } from "node:test";
+import Calculator from "./calculator";
+
+describe("Calculator 함수 테스트", () => {
+  const calculator = new Calculator();
+
+  it("sum 함수 테스트", () => {
+    expect(calculator.sum(1, 2)).toBe(3);
+  });
+
+  it("sub 함수 테스트", () => {
+    expect(calculator.sub(2, 1)).toBe(1);
+  });
+
+  it("mul 함수 테스트", () => {
+    expect(calculator.mul(2, 3)).toBe(6);
+  });
+
+  it("div 함수 테스트", () => {
+    expect(calculator.div(6, 3)).toBe(2);
+  });
+});
