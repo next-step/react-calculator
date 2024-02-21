6851f09bf887b92560e000fec652cd57b8158137
lomen-SeoJaeWan <sjw7324@lomen.kr>
2024-02-21 22:47:23 +0900

    feat: 숫자 입력 커스텀 훅 작성

--- src/hooks/useCalculator/index.js
+++ src/hooks/useCalculator/index.js
@@ -1,3 +1,22 @@
-const useCalculator = () => {};
+import { useState } from "react";
+
+const useCalculator = () => {
+  const [total, setTotal] = useState(0);
+
+  const update = (value) => {
+    setTotal((prev) => {
+      const updateValue = Number(prev + "" + value);
+
+      if (updateValue > 999) {
+        window.alert("숫자는 세 자리까지만 입력 가능합니다!");
+        return prev;
+      }
+
+      return updateValue;
+    });
+  };
+
+  return [total, update];
+};
 
 export default useCalculator;
