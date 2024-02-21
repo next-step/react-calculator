32061714da1c62676e5c4bf5e823bbaa2a5cdf3c
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-02-21 12:13:23 +0900

    0이 자릿수의 맨 앞에 오는 경우, 0을 뒤에 연속으로 추가할 수 없다.

--- src/components/NumberButton.tsx
+++ src/components/NumberButton.tsx
@@ -17,6 +17,13 @@ export default function NumberButton({
 }: NumberButtonProps) {
 	const alertMessage = '숫자는 세 자리까지만 입력 가능합니다!';
 	const isNotValidNumberLength = (number: string) => (number.length > 2);
+	const isNotValidZero = (preNumber: string, number: string) => {
+		if (number === '0') {
+			return preNumber.length === 0;
+		}
+
+		return false;
+	};
 
 	const handleClickNumber = (e: React.MouseEvent<HTMLButtonElement>) => {
 		const {value: number} = e.currentTarget;
@@ -27,12 +34,20 @@ export default function NumberButton({
 				return;
 			}
 
+			if (isNotValidZero(firstNumber, number)) {
+				return;
+			}
+
 			setFirstNumber(firstNumber + number);
 		}
 
 		if (operator) {
 			if (isNotValidNumberLength(secondNumber)) {
 				alert(alertMessage);
+				return;
+			}
+
+			if (isNotValidZero(secondNumber, number)) {
 				return;
 			}
 
