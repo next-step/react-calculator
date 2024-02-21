846cd7bfce1e55f713d85c724132aa3292bb0b86
bxxmi <bmi6638@gmail.com>
2024-02-21 23:22:38 +0900

    [config] index.tsx, app.tsx 연결

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  esbuild: {
-    jsxInject: `import React from 'react'`,
-  },
+  plugins: [react()],
 });
