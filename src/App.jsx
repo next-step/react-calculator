ecf5c6e37781ca6c87b088d68719b0d99219dd1a
lomen-SeoJaeWan <sjw7324@lomen.kr>
2024-02-21 23:12:57 +0900

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,39 @@
-import { useState } from "react";
+import useCalculator from "./hooks/useCalculator";
 
 function App() {
-  const [total, setTotal] = useState("0");
-
-  const handleClick = (e) => {
-    const value = e.target.id;
-
-    setTotal((prev) => {
-      if (value === "0" && prev === "0") return prev;
-      const update = prev === "0" ? value : prev + value;
-
-      if (update.length > 3) {
-        window.alert("숫자는 세 자리까지만 입력 가능합니다!");
-        return prev;
-      }
-
-      return update;
-    });
-  };
+  const [total, setTotal] = useCalculator("0");
 
   return (
     <div id="app">
       <h1 id="total">{total}</h1>
-      <button id="1" onClick={handleClick}>
+      <button id="1" onClick={() => setTotal(1)}>
         1
       </button>
-      <button id="2" onClick={handleClick}>
+      <button id="2" onClick={() => setTotal(2)}>
         2
       </button>
-      <button id="3" onClick={handleClick}>
+      <button id="3" onClick={() => setTotal(3)}>
         3
       </button>
-      <button id="4" onClick={handleClick}>
+      <button id="4" onClick={() => setTotal(4)}>
         4
       </button>
-      <button id="5" onClick={handleClick}>
+      <button id="5" onClick={() => setTotal(5)}>
         5
       </button>
-      <button id="6" onClick={handleClick}>
+      <button id="6" onClick={() => setTotal(6)}>
         6
       </button>
-      <button id="7" onClick={handleClick}>
+      <button id="7" onClick={() => setTotal(7)}>
         7
       </button>
-      <button id="8" onClick={handleClick}>
+      <button id="8" onClick={() => setTotal(8)}>
         8
       </button>
-      <button id="9" onClick={handleClick}>
+      <button id="9" onClick={() => setTotal(9)}>
         9
       </button>
-      <button id="0" onClick={handleClick}>
+      <button id="0" onClick={() => setTotal(0)}>
         0
       </button>
     </div>
--- src/utils/calculator.js
+++ src/utils/calculator.js
@@ -1,4 +1,26 @@
+const MAXIMUM = 999;
+const ERROR = {
+  maximum: "숫자는 세 자리까지만 입력 가능합니다!",
+  operator: "숫자를 먼저 입력한 후 연산자를 입력해주세요!",
+};
+
 class Calculator {
+  total: 0;
+
+  updateNumber(value) {
+    const updateValue = Number(this.total + "" + value);
+
+    if (updateValue > MAXIMUM) {
+      window.alert(ERROR.maximum);
+      return this.total;
+    }
+
+    this.total = updateValue;
+    return this.total;
+  }
+
+  updateCalculator(value) {}
+
   sum(a, b) {
     return a + b;
   }
