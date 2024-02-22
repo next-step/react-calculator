4f7d4d59d49cac5b82e46f047c175c13da51d84b
Hoseok Na <hsna7024@gmail.com>
2024-02-23 00:46:40 +0900

    feat: 2개의 피연산자를 받을 수 있도록 상태명 변경 및 화면 연동

--- src/Calculator/Calculator.tsx
+++ src/Calculator/Calculator.tsx
@@ -4,16 +4,23 @@ import OperationButtonList from './OperationButtonList'
 import { OPERATION } from './OperationButtonList/OperationButton/OperationButton.type'
 
 const Calculator = () => {
-  const [digit, setDigit] = useState(0)
+  const [leftOperand, setLeftOperand] = useState('')
   const [operation, setOperation] = useState<OPERATION | ''>('')
+  const [rightOperand, setRightOperand] = useState('')
 
   return (
     <section className="calculator">
-      <h1 id="total">{`${digit}${operation}`}</h1>
+      <h1 id="total">{`${leftOperand}${operation}${rightOperand}`}</h1>
       <DigitButtonList
-        onChange={(nextDigit: number) =>
-          setDigit((prevDigit) => Number(`${prevDigit}${nextDigit}`))
-        }
+        onChange={(digit: number) => {
+          if (!operation) {
+            setLeftOperand((prevLeftOperand) => `${prevLeftOperand}${digit}`)
+
+            return
+          }
+
+          setRightOperand((prevRightOperand) => `${prevRightOperand}${digit}`)
+        }}
       />
       <div className="modifiers subgrid">
         <button className="modifier">AC</button>
