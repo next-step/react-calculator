61e5a67553c271ed180959426788ee5d9b9dea70
lomen-SeoJaeWan <sjw7324@lomen.kr>
2024-02-23 00:22:24 +0900

    design: layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,65 @@ function App() {
 
   return (
     <div id="app">
-      <h1>{total}</h1>
-      <button onClick={() => update("1")}>1</button>
-      <button onClick={() => update("2")}>2</button>
-      <button onClick={() => update("3")}>3</button>
-      <button onClick={() => update("4")}>4</button>
-      <button onClick={() => update("5")}>5</button>
-      <button onClick={() => update("6")}>6</button>
-      <button onClick={() => update("7")}>7</button>
-      <button onClick={() => update("8")}>8</button>
-      <button onClick={() => update("9")}>9</button>
-      <button onClick={() => update("0")}>0</button>
+      <div className="calculator">
+        <h1 id="total">{total}</h1>
+        <div className="digits">
+          <button className="digit" onClick={() => update("9")}>
+            9
+          </button>
+          <button className="digit" onClick={() => update("8")}>
+            8
+          </button>
+          <button className="digit" onClick={() => update("7")}>
+            7
+          </button>
+          <button className="digit" onClick={() => update("6")}>
+            6
+          </button>
+          <button className="digit" onClick={() => update("5")}>
+            5
+          </button>
+          <button className="digit" onClick={() => update("4")}>
+            4
+          </button>
+          <button className="digit" onClick={() => update("3")}>
+            3
+          </button>
+          <button className="digit" onClick={() => update("2")}>
+            2
+          </button>
+          <button className="digit" onClick={() => update("1")}>
+            1
+          </button>
+          <button className="digit" onClick={() => update("0")}>
+            0
+          </button>
+        </div>
 
-      <button onClick={allClear}>AC</button>
+        <div className="modifiers subgrid">
+          <button className="modifier" onClick={allClear}>
+            AC
+          </button>
+        </div>
 
-      <button onClick={() => update("+")}>+</button>
-      <button onClick={() => update("-")}>-</button>
-      <button onClick={() => update("/")}>/</button>
-      <button onClick={() => update("X")}>X</button>
-
-      <button onClick={() => update("=")}>=</button>
+        <div className="operations subgrid">
+          <button className="operation" onClick={() => update("/")}>
+            /
+          </button>
+          <button className="operation" onClick={() => update("X")}>
+            X
+          </button>
+          <button className="operation" onClick={() => update("-")}>
+            -
+          </button>
+          <button className="operation" onClick={() => update("+")}>
+            +
+          </button>
+          <button className="operation" onClick={() => update("=")}>
+            =
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
