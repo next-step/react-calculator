2b8dcded1d38b1987f62fe8b2c012df63c5d29dd
lomen-SeoJaeWan <sjw7324@lomen.kr>
2024-02-22 23:35:18 +0900

    feat: 커스텀 훅 초기화 함수 작성

--- src/hooks/useCalculator/index.js
+++ src/hooks/useCalculator/index.js
@@ -1,15 +1,21 @@
 import { useState } from "react";
 import Calculator from "../../utils/calculator";
 
+const INITIAL = "0";
+
 const useCalculator = () => {
-  const [total, setTotal] = useState("0");
+  const [total, setTotal] = useState(INITIAL);
   const calculator = new Calculator();
 
   const update = (value) => {
     setTotal((prev) => calculator.update(value, prev));
   };
 
-  return [total, update];
+  const allClear = () => {
+    setTotal(INITIAL);
+  };
+
+  return [total, update, allClear];
 };
 
 export default useCalculator;
