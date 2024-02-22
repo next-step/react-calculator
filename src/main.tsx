a8f66bb857f5b9e8198b4eb8f0086709a2e43ef1
bytrustu <bytrustu@gmail.com>
2024-02-22 20:45:47 +0900

    test: 미션 요구사항 테스트 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,4 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import App from '@/App';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App />
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
