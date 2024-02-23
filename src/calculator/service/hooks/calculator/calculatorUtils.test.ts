06757550861651b89abb4983383f94b5db1a775c
ww8007 <wshmin1234@gmail.com>
2024-02-24 08:52:49 +0900

    test: useCalculator 내부에서 사용되는 유틸 함수에 대한 테스트 코드 추가

---
+++ src/calculator/service/hooks/calculator/calculatorUtils.test.ts
@@ -0,0 +1,71 @@
+import { describe, it, expect } from "vitest";
+import { CalculatorReceiver } from "../../commands/CalculatorCommands";
+import {
+	isValidExpressionAndUnderLimit,
+	parseFourBasicOperationsExpression,
+	performCalculation,
+	extractLastOperation
+} from "./calculatorUtils";
+
+describe("계산기 유틸리티 함수 테스트", () => {
+	const receiver = new CalculatorReceiver();
+
+	describe("isValidExpressionAndUnderLimit 함수 검증", () => {
+		it("숫자 뒤에 연산자를 입력할 수 있다", () => {
+			// Given: 현재 표현식이 '123'이고, 제한이 1000일 때
+			// When: '+' 연산자를 입력했을 때
+			// Then: 입력이 허용되어야 한다
+			expect(isValidExpressionAndUnderLimit("123", "+", 1000)).toBe(true);
+		});
+
+		it("연산자를 연속으로 입력할 수 없다", () => {
+			// Given: 현재 표현식이 '123+'이고, 제한이 1000일 때
+			// When: '+' 연산자를 다시 입력했을 때
+			// Then: 입력이 허용되지 않아야 한다
+			expect(isValidExpressionAndUnderLimit("123+", "+", 1000)).toBe(false);
+		});
+
+		it("입력된 숫자가 제한을 초과할 경우 입력을 막는다", () => {
+			// Given: 현재 표현식이 '999'이고, 제한이 1000일 때
+			// When: '9' 숫자를 추가로 입력했을 때
+			// Then: 입력이 허용되지 않아야 한다
+			expect(isValidExpressionAndUnderLimit("999", "9", 1000)).toBe(false);
+		});
+	});
+
+	describe("parseExpression 함수 검증", () => {
+		it("표현식을 숫자와 연산자로 올바르게 분리한다", () => {
+			// Given: '123+456-789' 표현식이 주어졌을 때
+			// When: 표현식을 파싱했을 때
+			// Then: ["123", "+", "456", "-", "789"]로 올바르게 분리되어야 한다
+			expect(parseFourBasicOperationsExpression("123+456-789")).toEqual([
+				"123",
+				"+",
+				"456",
+				"-",
+				"789"
+			]);
+		});
+	});
+
+	describe("performCalculation 함수 검증", () => {
+		it("주어진 파싱된 표현식에 대한 계산을 올바르게 수행한다", () => {
+			// Given: '10 + 20' 표현식이 주어졌을 때
+			// When: 계산을 수행했을 때
+			// Then: 결과값으로 30이 반환되어야 한다
+			receiver.setInput("0"); // 리시버를 0으로 초기화
+			const result = performCalculation(receiver, ["10", "+", "20"]);
+			expect(result).toBe(30);
+		});
+	});
+
+	describe("extractLastOperation 함수 검증", () => {
+		it("파싱된 표현식에서 마지막 연산을 올바르게 추출한다", () => {
+			// Given: "10 + 20 - 5" 표현식이 주어졌을 때
+			// When: 마지막 연산을 추출했을 때
+			// Then: { operator: "-", number: 5 }가 반환되어야 한다
+			const result = extractLastOperation(["10", "+", "20", "-", "5"]);
+			expect(result).toEqual({ operator: "-", number: 5 });
+		});
+	});
+});
