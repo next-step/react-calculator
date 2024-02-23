99f589698bdb2be7e1e93bc72f5513566fbbfab8
Yangseungchan <uvzone@naver.com>
2024-02-23 23:14:26 +0900

    feat: 계산기 모듈 완성, 테스트 코드 작성

---
+++ src/lib/calculator.test.ts
@@ -0,0 +1,28 @@
+import Calculator from '@/lib/calculator'
+
+describe('Calculator module test', () => {
+  test('+가 포함된 input에 대해서 더하기 연산을 한다', () => {
+    expect(new Calculator('12+3').calculate()).toBe(15)
+    expect(new Calculator('-12+3').calculate()).toBe(-9)
+  })
+
+  test('-가 포함된 input에 대해서 뺄셈 연산을 한다', () => {
+    expect(new Calculator('20-16').calculate()).toBe(4)
+    expect(new Calculator('12-16').calculate()).toBe(-4)
+  })
+
+  test('*가 포함된 input에 대해서 곱하기 연산을 한다', () => {
+    expect(new Calculator('14*3').calculate()).toBe(42)
+    expect(new Calculator('-14*3').calculate()).toBe(-42)
+  })
+
+  test('/가 포함된 Input에 대해서 나눗셈 연산을 한다', () => {
+    expect(new Calculator('14/2').calculate()).toBe(7)
+    expect(new Calculator('14/0').calculate()).toBe(Infinity)
+  })
+
+  test('피연산자가 빠진 채로 연산이 이뤄지면 앞의 숫자를 반환한다', () => {
+    expect(new Calculator('12+').calculate()).toBe(12)
+    expect(new Calculator('12*').calculate()).toBe(12)
+  })
+})
---
+++ src/lib/calculator.ts
@@ -0,0 +1,80 @@
+type Operator = '+' | '-' | '*' | '/'
+
+export default class Calculator {
+  static INIT_OPERAND = ''
+  static OPERATOR_REGEX = /[+\-*/]/
+
+  operands: number[]
+  operators: Operator[]
+  expressionArr: string[]
+
+  constructor(input: string) {
+    this.operands = []
+    this.operators = []
+    this.expressionArr = input.split('')
+    this.readExpression()
+  }
+
+  isOperand(input: string) {
+    return !Calculator.OPERATOR_REGEX.test(input)
+  }
+
+  isNonOperatorNegative(character: string, index: number) {
+    return index === 0 && character === '-'
+  }
+
+  addOperand(operand: string) {
+    this.operands.push(Number(operand))
+  }
+
+  readExpression() {
+    let operand = Calculator.INIT_OPERAND
+    for (const [index, character] of this.expressionArr.entries()) {
+      if (this.isOperand(character) || this.isNonOperatorNegative(character, index)) {
+        operand += character
+      } else {
+        this.operators.push(character as Operator)
+        if (operand.length > 0) {
+          this.addOperand(operand)
+          operand = Calculator.INIT_OPERAND
+        }
+      }
+    }
+    if (operand) {
+      this.addOperand(operand)
+    }
+  }
+  sum(operand1: number, operand2: number) {
+    return operand1 + operand2
+  }
+
+  subtract(operand1: number, operand2: number) {
+    return operand1 - operand2
+  }
+
+  multiply(operand1: number, operand2: number) {
+    return operand1 * operand2
+  }
+  divide(operand1: number, operand2: number) {
+    return operand1 / operand2
+  }
+
+  calculate() {
+    const operatorMap = {
+      '+': this.sum,
+      '-': this.subtract,
+      '*': this.multiply,
+      '/': this.divide,
+    }
+
+    while (this.operands.length >= 2 && this.operators.length > 0) {
+      const operand1 = this.operands.shift() as number
+      const operand2 = this.operands.shift() as number
+      const operatorMethod = operatorMap[this.operators.shift() as Operator]
+      const result = operatorMethod(operand1, operand2)
+      this.operands.unshift(result)
+    }
+
+    return this.operands[0]
+  }
+}
