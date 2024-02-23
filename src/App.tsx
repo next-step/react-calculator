c6732c1f37e3835704f5c1d94f1b275953a15ff5
ww8007 <wshmin1234@gmail.com>
2024-02-24 08:50:21 +0900

    feat: Calculator 비지니스 로직 및 컴포넌트 렌더링 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,23 @@
-import { useState } from "react";
-import "./App.css";
-import reactLogo from "./assets/react.svg";
+import { useCalculator } from "./calculator/service/hooks/calculator/useCalculator";
+import Calculator from "./calculator/ui/Calculator";
+import "./css/index.css";
+import "./css/reset.css";
 
 function App() {
-	const [count, setCount] = useState(0);
+	const { expression, handleInput, reset, calculateAndUpdateLastOperation } =
+		useCalculator();
 
 	return (
 		<>
-			<div>
-				<a href='https://react.dev' target='_blank'>
-					<img src={reactLogo} className='logo react' alt='React logo' />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className='card'>
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className='read-the-docs'>
-				Click on the Vite and React logos to learn more
-			</p>
+			<Calculator>
+				<Calculator.Display value={expression} />
+				<Calculator.Reset onClick={reset} />
+				<Calculator.Keypad onClick={handleInput} />
+				<Calculator.Operators
+					onClick={handleInput}
+					onCalculate={calculateAndUpdateLastOperation}
+				/>
+			</Calculator>
 		</>
 	);
 }
