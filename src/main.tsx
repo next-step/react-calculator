a1fa5eb16328d7ff4398778a22a1638e48e03369
hyunjinkim <71summernight@naver.com>
2024-02-25 00:21:25 +0900

    가운데 정렬을 위한 css id 추가

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <div id="app">
+      <App />
+    </div>
   </React.StrictMode>,
 );
