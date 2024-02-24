7ad9b42be67072ea325599fba45b9f6279889e5b
Gn0lee <jinho605@gmail.com>
2024-02-24 14:58:09 +0900

    feat : 이벤트 핸들러 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,46 @@
-import 'src/css/index.css';
+import useCalculator from 'src/hooks/useCalculator.ts';
 
 function App() {
+	const { enter, calculate, clear, display } = useCalculator();
+
+	const digits = ['9', '8', '7', '6', '5', '4', '3', '2', '1', '0'];
+
+	const operators = ['/', 'x', '-', '+'];
+
 	return (
 		<div className="calculator">
-			<h1 id="total">0</h1>
+			<h1 id="total">{display}</h1>
 			<div className="digits flex">
-				<button className="digit">9</button>
-				<button className="digit">8</button>
-				<button className="digit">7</button>
-				<button className="digit">6</button>
-				<button className="digit">5</button>
-				<button className="digit">4</button>
-				<button className="digit">3</button>
-				<button className="digit">2</button>
-				<button className="digit">1</button>
-				<button className="digit">0</button>
+				{digits.map(digit => (
+					<button className="digit" key={digit} onClick={() => enter(digit)}>
+						{digit}
+					</button>
+				))}
 			</div>
 			<div className="modifiers subgrid">
-				<button className="modifier">AC</button>
+				<button
+					className="modifier"
+					onClick={() => {
+						clear();
+					}}
+				>
+					AC
+				</button>
 			</div>
 			<div className="operations subgrid">
-				<button className="operation">/</button>
-				<button className="operation">X</button>
-				<button className="operation">-</button>
-				<button className="operation">+</button>
-				<button className="operation">=</button>
+				{operators.map(operator => (
+					<button className="operation" key={operator} onClick={() => enter(operator)}>
+						{operator}
+					</button>
+				))}
+				<button
+					className="operation"
+					onClick={() => {
+						calculate();
+					}}
+				>
+					=
+				</button>
 			</div>
 		</div>
 	);
