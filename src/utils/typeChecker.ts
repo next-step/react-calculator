60f39f793d361b901ca52a358a48282a17d3e3d6
bytrustu <bytrustu@gmail.com>
2024-02-24 15:12:36 +0900

    refactor: validOperation함수의 타입 단언을 제거하도록 개선

--- src/utils/typeChecker.ts
+++ src/utils/typeChecker.ts
@@ -1,7 +1,7 @@
 import { Operation } from '@/types';
 
-const operationSet = new Set(Object.values(Operation));
+const operationSet: Set<string> = new Set(Object.values(Operation));
 
 export const typeChecker = {
-  validOperation: (value: string): value is Operation => operationSet.has(value as Operation),
+  validOperation: (value: string): value is Operation => operationSet.has(value),
 };
