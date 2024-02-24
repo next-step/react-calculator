6f7f32ce07e204f9c19253f89e271b946221d91e
jiyeon <ejy1105@gmail.com>
2024-02-24 18:21:10 +0900

    fix: 로직 단순화

--- src/components/Operator.tsx
+++ src/components/Operator.tsx
@@ -1,24 +1,14 @@
-import { Operation } from '../hooks/useCalculator'
+import { Operation } from '../constants/operation'
 import styled from '../css/index.module.css'
 
 const Operator = ({ operation }: { operation: (param: Operation) => void }) => {
   return (
     <div className={`${styled.operations} ${styled.subgrid}`}>
-      <button className={styled.operation} onClick={() => operation(Operation.DIVIDED)}>
-        {Operation.DIVIDED}
-      </button>
-      <button className={styled.operation} onClick={() => operation(Operation.MULTIPLE)}>
-        {Operation.MULTIPLE}
-      </button>
-      <button className={styled.operation} onClick={() => operation(Operation.MINUS)}>
-        {Operation.MINUS}
-      </button>
-      <button className={styled.operation} onClick={() => operation(Operation.SUM)}>
-        {Operation.SUM}
-      </button>
-      <button className={styled.operation} onClick={() => operation(Operation.EQUAL)}>
-        {Operation.EQUAL}
-      </button>
+      {Object.entries(Operation).map(([key, value]) => (
+        <button key={key} className={styled.operation} onClick={() => operation(value)}>
+          {value}
+        </button>
+      ))}
     </div>
   )
 }
