cb94c9c756e3dbceee998893273a8ca261976114
Kangjun Lee <me@gangjun.dev>
2024-02-24 23:55:48 +0900

    feat: use-calculator 함수를 구현

--- src/constants.ts
+++ src/constants.ts
@@ -5,6 +5,9 @@ export const Operators = {
   Divide: "/",
 } as const;
 
+export type OperatorKeys = keyof typeof Operators;
+export type OperatorValues = (typeof Operators)[OperatorKeys];
+
 export const Messages = {
   InputNumberFirst: "숫자를 먼저 입력한 후 연산자를 입력해주세요!",
   InputOperatorOnce: "연산자는 한번만 입력할 수 있습니다.",
--- src/hooks/use-calculator.ts
+++ src/hooks/use-calculator.ts
@@ -1,29 +1,83 @@
 import { useMemo, useState } from "react";
+import { Messages, OperatorValues, Operators } from "../constants";
+
+const OperatorMap: Record<OperatorValues, (a: number, b: number) => number> = {
+  [Operators.Plus]: (a: number, b: number) => a + b,
+  [Operators.Minus]: (a: number, b: number) => a - b,
+  [Operators.Multiply]: (a: number, b: number) => a * b,
+  [Operators.Divide]: (a: number, b: number) => Math.floor(a / b),
+};
 
 export const useCalculator = () => {
   const [firstNum, setFirstNum] = useState(0);
   const [secondNum, setSecondNum] = useState(0);
-  const [operator, setOperator] = useState("");
+  const [operator, setOperator] = useState<OperatorValues | null>(null);
 
   const displayText = useMemo(() => {
     let result = firstNum.toString();
 
-    if (operator) {
-      result += ` ${operator}`;
-    }
-
-    if (secondNum) {
-      result += ` ${secondNum}`;
-    }
+    if (operator) result += ` ${operator}`;
+    if (secondNum !== 0) result += ` ${secondNum}`;
 
     return result;
   }, [firstNum, secondNum, operator]);
 
-  const handleNumberInput = (num: number) => {};
-  const handleSetOperator = (op: string) => {};
-  const handleCalculate = () => {};
+  const handleNumberInput = (input: number) => {
+    if (!operator) {
+      if (firstNum.toString().length >= 3) {
+        window.alert(Messages.MaxInputLength);
+        return;
+      }
 
-  const handleReset = () => {};
+      if (firstNum === 0) {
+        setFirstNum(input);
+        return;
+      }
+
+      setFirstNum(parseInt(`${firstNum}${input}`, 10));
+    } else {
+      if (secondNum.toString().length >= 3) {
+        window.alert(Messages.MaxInputLength);
+        return;
+      }
+
+      if (secondNum === 0) {
+        setSecondNum(input);
+        return;
+      }
+
+      setSecondNum(parseInt(`${secondNum}${input}`, 10));
+    }
+  };
+
+  const handleSetOperator = (input: OperatorValues) => {
+    if (firstNum === 0 && secondNum === 0) {
+      window.alert(Messages.InputNumberFirst);
+      return;
+    }
+
+    if (operator) {
+      window.alert(Messages.InputOperatorOnce);
+      return;
+    }
+
+    setOperator(input);
+  };
+
+  const handleCalculate = () => {
+    if (!operator) return;
+    const result = OperatorMap[operator](firstNum, secondNum);
+
+    setFirstNum(result);
+    setSecondNum(0);
+    setOperator(null);
+  };
+
+  const handleReset = () => {
+    setFirstNum(0);
+    setSecondNum(0);
+    setOperator(null);
+  };
 
   return {
     displayText,
