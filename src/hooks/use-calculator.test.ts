257574ca0cfdf3aab2e8237c8484963c6af99c91
Kangjun Lee <me@gangjun.dev>
2024-02-24 23:54:52 +0900

    test: hook이 test에서 정상적으로 랜더링되지 않는문제

--- src/hooks/use-calculator.test.ts
+++ src/hooks/use-calculator.test.ts
@@ -1,161 +1,163 @@
-import { renderHook } from "@testing-library/react-hooks";
+import { RenderHookResult, renderHook } from "@testing-library/react";
 import { useCalculator } from "./use-calculator";
 import { Messages, Operators } from "../constants";
+import { act } from "react-dom/test-utils";
 
 describe("useCalculator", () => {
-  let current: ReturnType<typeof useCalculator>;
+  let result: RenderHookResult<
+    ReturnType<typeof useCalculator>,
+    unknown
+  >["result"];
 
   beforeEach(() => {
     window.alert = jest.fn();
-    const {
-      result: { current: currentInstance },
-    } = renderHook(() => useCalculator());
-    current = currentInstance;
+    const { result: resultInstance } = renderHook(() => useCalculator());
+    result = resultInstance;
   });
 
   test("처음 렌더링 시 displayText는 0이어야 합니다", () => {
-    expect(current.displayText).toBe("0");
+    expect(result.current.displayText).toBe("0");
   });
 
   describe("숫자 입력", () => {
-    test("handleNumber를 통해 숫자를 입력합니다", () => {
-      current.handleNumberInput(1);
-      expect(current.displayText).toBe("1");
+    test("handleNumber를 통해 숫자를 입력합니다", async () => {
+      act(() => result.current.handleNumberInput(1));
+      expect(result.current.displayText).toBe("1");
 
-      current.handleNumberInput(2);
-      expect(current.displayText).toBe("12");
+      act(() => result.current.handleNumberInput(2));
+      expect(result.current.displayText).toBe("12");
 
-      current.handleNumberInput(3);
-      expect(current.displayText).toBe("123");
+      act(() => result.current.handleNumberInput(3));
+      expect(result.current.displayText).toBe("123");
     });
 
     test("숫자는 0으로 시작할 수 없습니다", () => {
-      current.handleNumberInput(0);
+      act(() => {
+        result.current.handleNumberInput(0);
+      });
 
-      expect(current.displayText).toBe("0");
+      expect(result.current.displayText).toBe("0");
     });
 
     test("숫자는 최대 3자리까지만 입력할 수 있습니다", () => {
-      current.handleNumberInput(1);
-      current.handleNumberInput(2);
-      current.handleNumberInput(3);
-      current.handleNumberInput(4);
+      act(() => result.current.handleNumberInput(1));
+      act(() => result.current.handleNumberInput(2));
+      act(() => result.current.handleNumberInput(3));
+      act(() => result.current.handleNumberInput(4));
 
-      expect(current.displayText).toBe("123");
+      expect(result.current.displayText).toBe("123");
       expect(window.alert).toHaveBeenCalledWith(Messages.MaxInputLength);
     });
 
     test("연산자 입력 후에도 숫자는 최대 3자리까지만 입력할 수 있습니다", () => {
-      current.handleNumberInput(1);
-      current.handleSetOperator(Operators.Plus);
-      current.handleNumberInput(2);
-      current.handleNumberInput(3);
-      current.handleNumberInput(4);
-      current.handleNumberInput(5);
+      act(() => result.current.handleNumberInput(1));
+      act(() => result.current.handleSetOperator(Operators.Plus));
+      act(() => result.current.handleNumberInput(2));
+      act(() => result.current.handleNumberInput(3));
+      act(() => result.current.handleNumberInput(4));
+      act(() => result.current.handleNumberInput(5));
 
-      expect(current.displayText).toBe("1 + 234");
+      expect(result.current.displayText).toBe("1 + 234");
       expect(window.alert).toHaveBeenCalledWith(Messages.MaxInputLength);
     });
 
     describe("연산자 입력", () => {
       test("handleSetOperator를 통해 연산자를 입력합니다", () => {
-        current.handleNumberInput(1);
-        current.handleSetOperator(Operators.Plus);
+        act(() => result.current.handleNumberInput(1));
+        act(() => result.current.handleSetOperator(Operators.Plus));
 
-        expect(current.displayText).toBe("1 +");
+        expect(result.current.displayText).toBe("1 +");
       });
     });
 
     test("연산자는 숫자 입력 후에만 입력할 수 있습니다", () => {
-      current.handleSetOperator(Operators.Plus);
+      act(() => result.current.handleSetOperator(Operators.Plus));
 
-      expect(current.displayText).toBe("0");
-      expect(window.alert).toHaveBeenCalledWith(Messages.InputNumberFirst);
-    });
-
-    test("이미 연산자가 있더라도, 연산자는 숫자 입력 후에만 입력할 수 있습니다", () => {
-      current.handleNumberInput(1);
-      current.handleSetOperator(Operators.Plus);
-      current.handleNumberInput(2);
-      current.handleSetOperator(Operators.Plus);
-
-      expect(current.displayText).toBe("1 + 2");
-
+      expect(result.current.displayText).toBe("0");
       expect(window.alert).toHaveBeenCalledWith(Messages.InputNumberFirst);
     });
 
     // github.com/next-step/react-calculator 데모의 경우 연산자가 중복으로 입력이 가능하지만,
     // 첫번째 연산자까지만 계산이 되기에, 연산자 중복 입력을 막도록 구현하였습니다.
     test("이미 연산자가 있으면, 연산자를 입력할 수 없습니다", () => {
-      current.handleNumberInput(1);
-      current.handleSetOperator(Operators.Plus);
-      current.handleSetOperator(Operators.Minus);
+      act(() => result.current.handleNumberInput(1));
+      act(() => result.current.handleSetOperator(Operators.Plus));
+      act(() => result.current.handleSetOperator(Operators.Minus));
 
-      expect(current.displayText).toBe("1 +");
+      expect(result.current.displayText).toBe("1 +");
       expect(window.alert).toHaveBeenCalledWith(Messages.InputOperatorOnce);
     });
   });
 
   describe("handleCalculate를 통해 계산을 수행합니다", () => {
-    test("덧셈을 수행합니다", () => {
-      current.handleNumberInput(1);
-      current.handleSetOperator(Operators.Minus);
-      current.handleNumberInput(2);
-      current.handleCalculate();
+    test("연산자가 없는 경우 계산을 수행하지 않습니다", () => {
+      act(() => result.current.handleNumberInput(1));
+      act(() => result.current.handleCalculate());
 
-      expect(current.displayText).toBe("3");
+      expect(result.current.displayText).toBe("1");
+    });
+
+    test("덧셈을 수행합니다", () => {
+      act(() => result.current.handleNumberInput(1));
+      act(() => result.current.handleSetOperator(Operators.Plus));
+      act(() => result.current.handleNumberInput(2));
+      act(() => result.current.handleCalculate());
+
+      expect(result.current.displayText).toBe("3");
     });
 
     test("뺄셈을 수행합니다", () => {
-      current.handleNumberInput(1);
-      current.handleSetOperator(Operators.Minus);
-      current.handleNumberInput(2);
-      current.handleCalculate();
+      act(() => result.current.handleNumberInput(1));
+      act(() => result.current.handleSetOperator(Operators.Minus));
+      act(() => result.current.handleNumberInput(2));
+      act(() => result.current.handleCalculate());
 
-      expect(current.displayText).toBe("-1");
+      expect(result.current.displayText).toBe("-1");
     });
 
     test("곱셈을 수행합니다", () => {
-      current.handleNumberInput(2);
-      current.handleSetOperator(Operators.Multiply);
-      current.handleNumberInput(3);
-      current.handleCalculate();
+      act(() => result.current.handleNumberInput(2));
+      act(() => result.current.handleSetOperator(Operators.Multiply));
+      act(() => result.current.handleNumberInput(3));
+      act(() => result.current.handleCalculate());
 
-      expect(current.displayText).toBe("6");
+      expect(result.current.displayText).toBe("6");
     });
 
     test("나눗셈을 수행합니다", () => {
-      current.handleNumberInput(10);
-      current.handleSetOperator(Operators.Divide);
-      current.handleNumberInput(2);
-      current.handleCalculate();
+      act(() => result.current.handleNumberInput(10));
+      act(() => result.current.handleSetOperator(Operators.Divide));
+      act(() => result.current.handleNumberInput(2));
+      act(() => result.current.handleCalculate());
 
-      expect(current.displayText).toBe("5");
+      expect(result.current.displayText).toBe("5");
     });
 
     test("나눌 수 없는경우 문자를 표시합니다", () => {
-      current.handleNumberInput(1);
-      current.handleSetOperator(Operators.Divide);
-      current.handleNumberInput(0);
-      current.handleCalculate();
+      act(() => result.current.handleNumberInput(1));
+      act(() => result.current.handleSetOperator(Operators.Divide));
+      act(() => result.current.handleNumberInput(0));
+      act(() => result.current.handleCalculate());
 
-      expect(current.displayText).toBe("Infinity");
+      expect(result.current.displayText).toBe("Infinity");
     });
 
     test("계산 결과값은 소숫점 이하는 버림합니다", () => {
-      current.handleNumberInput(10);
-      current.handleSetOperator(Operators.Divide);
-      current.handleNumberInput(3);
-      current.handleCalculate();
+      act(() => result.current.handleNumberInput(10));
+      act(() => result.current.handleSetOperator(Operators.Divide));
+      act(() => result.current.handleNumberInput(3));
+      act(() => result.current.handleCalculate());
 
-      expect(current.displayText).toBe("3");
+      expect(result.current.displayText).toBe("3");
     });
   });
 
   test("handleReset을 통해 초기화합니다", () => {
-    current.handleNumberInput(1);
-    current.handleReset();
+    act(() => {
+      result.current.handleNumberInput(1);
+      result.current.handleReset();
+    });
 
-    expect(current.displayText).toBe("0");
+    expect(result.current.displayText).toBe("0");
   });
 });
