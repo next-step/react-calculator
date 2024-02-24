0119093c2a9c806f20faa53a4668c587d2cd362e
sonsurim <surim014@naver.com>
2024-02-25 02:11:35 +0900

    feat: implement the multiply method of calculator

--- src/utils/calculator.ts
+++ src/utils/calculator.ts
@@ -30,6 +30,15 @@ export class Calculator {
     this.#value = Object.is(result, -0) ? 0 : result
   }
 
+  multiply(a: number, b: number) {
+    if(!(isNumber(a) && isNumber(a))) {
+      throw new Error(ERROR_MESSAGE.NOT_VALID_NUMBER)
+    }
+
+    const result = Math.trunc(a * b)
+    this.#value = Object.is(result, -0) ? 0 : result
+  }
+
   clear() {
     this.#value = 0
   }
