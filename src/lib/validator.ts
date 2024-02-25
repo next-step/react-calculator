1dc22e27e4d5a9571c025c9033349a61855642dc
Yangseungchan <uvzone@naver.com>
2024-02-25 15:05:46 +0900

    refactor: validator 클래스 로직 개선 및 간소화

--- src/lib/validator.ts
+++ src/lib/validator.ts
@@ -10,72 +10,54 @@ import {
 
 export default class Validator {
   expression: string
-  splitsByOperators: string[]
 
   constructor(expression: string) {
     this.expression = expression
-    this.splitsByOperators = expression.split(OPERATORS_REGEX)
     this.validateExpression()
   }
 
-  isOnlyNumber() {
-    return !Number.isNaN(Number(this.expression))
+  isOnlyOperand(expression: string) {
+    return !Number.isNaN(Number(expression))
   }
 
-  hasSuccessiveOperators() {
-    return this.expression
+  isOnlyOperator(expression: string) {
+    return (
+      expression === PLUS ||
+      expression == SUBTRACT ||
+      expression === MULTIPLY ||
+      expression === DIVIDE
+    )
+  }
+
+  hasSuccessiveOperators(expression: string) {
+    return expression
       .split(OPERATORS_REGEX)
       .some(
         (value, index, splitResult) => value === '' && index > 0 && splitResult[index - 1] === '',
       )
   }
 
-  isWithinMaxDigits(value: number, maxDigit: number = 3) {
+  isInvalidNumberRange(value: number, maxDigit: number = 3) {
     const maxValue = Math.pow(10, maxDigit) - 1
     const minValue = -1 * maxValue
-    return minValue <= value && value <= maxValue
+    return value < minValue || value > maxValue
   }
 
-  isInvalidNumberRange(num: number) {
-    return !this.isWithinMaxDigits(num)
-  }
-
-  isSingleNegativeNumber() {
-    return this.expression[0] === SUBTRACT && this.splitsByOperators.length === 2
-  }
-
-  getOperands() {
-    return this.splitsByOperators.filter(split => split !== '').map(split => Number(split))
-  }
-
-  isOperandsInvalid() {
-    return this.getOperands().some(operand => this.isInvalidNumberRange(operand))
+  isOperandsInvalid(expression: string) {
+    const operands = expression
+      .split(OPERATORS_REGEX)
+      .filter(split => split !== '')
+      .map(split => Number(split))
+    return operands.some(operand => this.isInvalidNumberRange(operand))
   }
 
   validateExpression() {
-    if (this.isOnlyNumber()) {
-      if (this.isInvalidNumberRange(Number(this.expression))) {
-        throw new Error(THREE_DIGIT_LIMIT_ERROR_MESSAGE)
-      }
-    } else {
-      if (
-        this.expression === PLUS ||
-        this.expression == SUBTRACT ||
-        this.expression === MULTIPLY ||
-        this.expression === DIVIDE
-      ) {
-        throw new Error(INPUT_NUMBER_FIRST_ERROR_MESSAGE)
-      } else if (this.hasSuccessiveOperators()) {
-        throw new Error(INPUT_NUMBER_FIRST_ERROR_MESSAGE)
-      } else if (
-        this.isSingleNegativeNumber() &&
-        this.isInvalidNumberRange(Number(this.expression))
-      ) {
-        throw new Error(THREE_DIGIT_LIMIT_ERROR_MESSAGE)
-      } else if (this.isOperandsInvalid()) {
-        throw new Error(THREE_DIGIT_LIMIT_ERROR_MESSAGE)
-      }
-    }
+    if (this.isOnlyOperator(this.expression)) throw new Error(INPUT_NUMBER_FIRST_ERROR_MESSAGE)
+
+    if (this.hasSuccessiveOperators(this.expression))
+      throw new Error(INPUT_NUMBER_FIRST_ERROR_MESSAGE)
+
+    if (this.isOperandsInvalid(this.expression)) throw new Error(THREE_DIGIT_LIMIT_ERROR_MESSAGE)
     return true
   }
 }
