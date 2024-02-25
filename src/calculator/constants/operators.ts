ce89e3c6129e37749f31e5fe90451ca4f1d9e6ab
ww8007 <wshmin1234@gmail.com>
2024-02-26 04:28:55 +0900

    fix: as const 사용 및 OPERATORS_ARRAY 타입 으로부터 타입 정보 전달 되게 수정

--- src/calculator/constants/operators.ts
+++ src/calculator/constants/operators.ts
@@ -1,5 +1,10 @@
-export const OPERATORS = new Set(["/", "X", "-", "+"]);
-export type OPERATORS_TYPE = "X" | "+" | "-" | "/";
+const OPERATORS_ARRAY = ["/", "X", "-", "+"] as const;
+
+export const OPERATORS = new Set<(typeof OPERATORS_ARRAY)[number]>(
+	OPERATORS_ARRAY
+);
+
+export type OPERATORS_TYPE = (typeof OPERATORS_ARRAY)[number];
 
 export const OPERATORS_MAP: Record<OPERATORS_TYPE, string> = {
 	X: "*",
@@ -9,5 +14,7 @@ export const OPERATORS_MAP: Record<OPERATORS_TYPE, string> = {
 };
 
 export const isOperator = (value: string): value is OPERATORS_TYPE => {
-	return ["+", "-", "X", "/"].includes(value);
+	return OPERATORS.has(value as OPERATORS_TYPE);
 };
+
+export const NUMBERS_AND_OPERATORS = /(\d+)|([+\-X\\/])/g;
