c58c2b9bde924dceff9a13cb8acb8373dbdfbcfa
gaeun <knospe1@naver.com>
2024-02-25 17:24:19 +0900

    fix: fix to able second num is negative

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -2,9 +2,11 @@ import useCalculator from '../hooks/useCalculator';
 import Key from './key/KeyButton';
 
 export default function Calculator() {
-  const { num1, num2, operator, inputNumber, clear, inputOperator } = useCalculator();
+  const { num1, num2, isNum2Negative, operator, inputNumber, clear, inputOperator } =
+    useCalculator();
 
-  const message = num1 === Infinity ? '오류' : `${num1}${operator ?? ''}${num2 ?? ''}`;
+  const messageOfNum2 = isNum2Negative ? `(-${num2 ? `${num2})` : ''}` : num2 ?? '';
+  const message = num1 === Infinity ? '오류' : `${num1}${operator ?? ''}${messageOfNum2}`;
   return (
     <div className="calculator">
       <h1 id="total">{message}</h1>
--- src/hooks/useCalculator.ts
+++ src/hooks/useCalculator.ts
@@ -5,13 +5,15 @@ import { makeOperate } from '../utils/calculator';
 interface CalculatorState {
   num1: number;
   num2?: number;
+  isNum2Negative?: boolean;
   operator?: OperatorType;
 }
 
 const INITIAL_CALCULATOR_STATE: CalculatorState = { num1: 0 };
 
 export default function useCalculator() {
-  const [{ num1, num2, operator }, setState] = useState<CalculatorState>(INITIAL_CALCULATOR_STATE);
+  const [{ num1, num2, operator, isNum2Negative }, setState] =
+    useState<CalculatorState>(INITIAL_CALCULATOR_STATE);
 
   const inputNumber = useCallback(
     (num: number) => {
@@ -27,6 +29,7 @@ export default function useCalculator() {
         alert('숫자는 세 자리까지만 입력 가능합니다!');
         return;
       }
+
       const newText = (inputText ?? '') + num.toString();
       setState(prev =>
         isFirstNum ? { ...prev, num1: parseInt(newText) } : { ...prev, num2: parseInt(newText) },
@@ -44,17 +47,23 @@ export default function useCalculator() {
       const shouldCalculate = input === '=' && num2 !== undefined && operator;
       if (shouldCalculate) {
         const operate = makeOperate(operator);
-        const result = operate(num1, num2);
+        const result = operate(num1, isNum2Negative ? -num2 : num2);
         setState({ num1: result });
         return;
       }
+
+      if (input === '-' && operator !== undefined) {
+        setState(prev => ({ ...prev, isNum2Negative: true }));
+        return;
+      }
+
       if (num2) {
         alert('두 수의 연산만 가능합니다!');
         return;
       }
       setState(prev => ({ ...prev, operator: input }));
     },
-    [num1, num2, operator],
+    [num1, num2, operator, isNum2Negative],
   );
-  return { num1, num2, operator, inputNumber, clear, inputOperator };
+  return { num1, num2, isNum2Negative, operator, inputNumber, clear, inputOperator };
 }
