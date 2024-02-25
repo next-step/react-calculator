85e0055253681da67a3e3a1055b700f9f2ea4fc7
Pengoose <pengoose_dev@naver.com>
2024-02-25 11:48:13 +0900

    refactor: useCalculator 구조 변경

--- src/hooks/useCalculator.ts
+++ src/hooks/useCalculator.ts
@@ -1,85 +1,116 @@
 import { useState } from 'react';
 import { BUTTON } from '../components/button/button.constant';
-import { Digit, Modifier, Operator } from '../components/button/button.type';
+import { Modifier, Operator } from '../components/button/button.type';
 
 export const useCalculator = () => {
-  const [numbers, setNumbers] = useState<{
+  const [state, setState] = useState<{
     x: number;
     y: number;
     result: number;
+    operator: Operator | null;
   }>({
     x: 0,
     y: 0,
     result: 0,
+    operator: null,
   });
 
-  const [operator, setOperator] = useState<Operator | null>(null);
+  const handleDigit = (value: number) => {
+    if (!state.operator) {
+      if (state.x * 10 > 999) {
+        alert('숫자는 세자리 이하로 입력해주세요');
+      }
 
-  const handleDigit = (value: Digit) => {
-    if (!operator) {
-      setNumbers((prev) => ({ ...prev, x: prev.x * 10 + value }));
+      setState((prev) => ({ ...prev, x: prev.x * 10 + value }));
 
       return;
     }
 
-    setNumbers((prev) => ({ ...prev, y: prev.y * 10 + value }));
+    if (state.y * 10 > 999) {
+      alert('숫자는 세자리 이하로 입력해주세요');
+    }
+
+    setState((prev) => ({ ...prev, y: prev.y * 10 + value }));
+  };
+
+  const reset = () => {
+    setState({
+      x: 0,
+      y: 0,
+      result: 0,
+      operator: null,
+    });
   };
 
   const handleModifier = (value: Modifier) => {
     if (value === BUTTON.MODIFIER.CHILDREN.AC.VALUE) {
-      setNumbers({
-        x: 0,
-        y: 0,
-        result: 0,
-      });
-      setOperator(null);
+      reset();
     }
   };
 
-  const handleOperator = (value: Operator) => {
-    if (operator) {
-      setNumbers((prev) => {
+  const calculate = (value: Operator) => {
+    if (value === BUTTON.OPERATION.CHILDREN.EQUAL.VALUE) {
+      setState((prev) => {
         let result = 0;
 
-        switch (operator) {
-          case BUTTON.OPERATION.CHILDREN.ADD.VALUE:
+        switch (state.operator) {
+          case BUTTON.OPERATION.CHILDREN.ADD.VALUE: {
             result = prev.x + prev.y;
-            break;
 
-          case BUTTON.OPERATION.CHILDREN.SUBTRACT.VALUE:
+            break;
+          }
+
+          case BUTTON.OPERATION.CHILDREN.SUBTRACT.VALUE: {
             result = prev.x - prev.y;
-            break;
 
-          case BUTTON.OPERATION.CHILDREN.MULTIPLY.VALUE:
+            break;
+          }
+
+          case BUTTON.OPERATION.CHILDREN.MULTIPLY.VALUE: {
             result = prev.x * prev.y;
-            break;
 
-          case BUTTON.OPERATION.CHILDREN.DIVIDE.VALUE:
+            break;
+          }
+
+          case BUTTON.OPERATION.CHILDREN.DIVIDE.VALUE: {
             result = Math.round((prev.x / prev.y) * 10) / 10;
-            break;
 
-          default:
             break;
+          }
+
+          default: {
+            break;
+          }
         }
 
         return {
           x: result,
           y: 0,
           result,
+          operator: null,
         };
       });
     }
+  };
 
-    setOperator(value);
+  console.log(state);
+  const handleOperator = (value: Operator) => {
+    if (value !== BUTTON.OPERATION.CHILDREN.EQUAL.VALUE) {
+      setState((prev) => ({ ...prev, operator: value }));
+
+      return;
+    }
+
+    calculate(value);
   };
 
   return {
-    result: numbers.result,
+    result: state.result,
     current: {
-      x: numbers.x,
-      y: numbers.y,
+      x: state.x,
+      y: state.y,
     },
-    operator,
+    operator: state.operator,
     handleDigit,
     handleOperator,
     handleModifier,
