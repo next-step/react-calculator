1f877c960c2ca08cc392eea3d175ed86bcb3afa1
gaeun <knospe1@naver.com>
2024-02-25 17:27:23 +0900

    fix: remove to drop decimal point when multiply

--- src/utils/calculator.ts
+++ src/utils/calculator.ts
@@ -4,7 +4,7 @@ type OPERATE = (num1: number, num2: number) => number;
 
 const add: OPERATE = (num1: number, num2: number) => num1 + num2;
 const substract: OPERATE = (num1: number, num2: number) => num1 - num2;
-const multiply: OPERATE = (num1: number, num2: number) => Math.floor(num1 * num2);
+const multiply: OPERATE = (num1: number, num2: number) => num1 * num2;
 const divide: OPERATE = (num1: number, num2: number) => Math.floor(num1 / num2);
 
 export const makeOperate = (operator: OperatorType) => {
