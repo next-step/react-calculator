fc03be85481f79b92d51c01bd6cc6b592c7ec4cd
Yangseungchan <uvzone@naver.com>
2024-02-25 11:44:48 +0900

    fix: validator error case 테스트코드 수정

--- src/lib/validator.test.ts
+++ src/lib/validator.test.ts
@@ -3,35 +3,33 @@ import Validator from './validator'
 
 describe('checkValidInput 검증', () => {
   test('숫자만 있는 문자열이지만 그 숫자가 유효한 범위의 숫자가 아니면 THREE_DIGIT_LIMIT_ERROR_MESSAGE가 난다', () => {
-    try {
-      new Validator('1234').validateExpression()
-    } catch (error) {
-      expect((error as Error)?.message).toBe(THREE_DIGIT_LIMIT_ERROR_MESSAGE)
-    }
+    expect(() => new Validator('1234').validateExpression()).toThrowError(
+      THREE_DIGIT_LIMIT_ERROR_MESSAGE,
+    )
   })
 
   test('연산자만 존재하는 문자열이면 INPUT_NUMBER_FIRST_ERROR_MESSAGE가 난다', () => {
-    try {
-      new Validator('+').validateExpression()
-    } catch (error) {
-      expect((error as Error)?.message).toBe(INPUT_NUMBER_FIRST_ERROR_MESSAGE)
-    }
+    expect(() => new Validator('+').validateExpression()).toThrowError(
+      INPUT_NUMBER_FIRST_ERROR_MESSAGE,
+    )
   })
 
   test('연산자가 연속해서 나타나는 표현식은 INPUT_NUMBER_FIRST_ERROR_MESSAGE가 난다', () => {
-    try {
-      new Validator('45+-').validateExpression()
-    } catch (error) {
-      expect((error as Error)?.message).toBe(INPUT_NUMBER_FIRST_ERROR_MESSAGE)
-    }
+    expect(() => new Validator('45+-').validateExpression()).toThrowError(
+      INPUT_NUMBER_FIRST_ERROR_MESSAGE,
+    )
   })
 
   test('음수가 입력될때 유효한 범위가 아니면 THREE_DIGIT_LIMIT_ERROR_MESSAGE가 난다', () => {
-    try {
-      new Validator('-9999').validateExpression()
-    } catch (error) {
-      expect((error as Error)?.message).toBe(THREE_DIGIT_LIMIT_ERROR_MESSAGE)
-    }
+    expect(() => new Validator('-9999').validateExpression()).toThrowError(
+      THREE_DIGIT_LIMIT_ERROR_MESSAGE,
+    )
+  })
+
+  test('두번째 이상의 피연산자에 유효하지 않은 범위의 숫자가 붙으면 THREE_DIGIT_LIMIT_ERROR_MESSAGE가 발생한다', () => {
+    expect(() => new Validator('88*8888').validateExpression()).toThrowError(
+      THREE_DIGIT_LIMIT_ERROR_MESSAGE,
+    )
   })
 
   test('유효한 경우 예외 테스트 1', () => {
