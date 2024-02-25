6f5439ae8374fdf5cb7e3399ef3c197115d4924a
ki moon Kim <kimoonkim@kiui-MacBookPro-2.local>
2024-02-25 12:23:06 +0900

    feat: 연산 기능 개발, AC, Infinity시 오류표출 기능 구현 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,106 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { NumberButton } from "./components/NumberButton";
 import { OperationButton } from "./components/OperationButton";
+import { OPERATION_REGEX } from "./constants";
 import "./css/index.css";
 import { EOPERATIONS } from "./enums";
 import { CALCULATOR_NUMBERS } from "./helper";
 
 function App() {
-  /**현재 입력받고 있는 숫자 */
-  const [currNumber, setCurrNumber] = useState(0);
-  const handleClickButton = (value: number | keyof typeof EOPERATIONS) => {
-    if (typeof value === "number") {
-      checkNumberLength(value);
+  const [inputtedTotalValue, setInputtedTotalValue] = useState<string>("0");
+  const currNumber = useRef<string>();
+
+  const handleClickNumberButton = (value: number) => {
+    const _currNumber = currNumber.current;
+    if (_currNumber?.length === 3) {
+      alert("숫자는 세 자리까지만 입력 가능합니다!");
+      return;
+    }
+    currNumber.current = _currNumber
+      ? _currNumber + value.toString()
+      : value.toString();
+
+    setInputtedTotalValue((prev) =>
+      prev === "0" || prev === "오류" ? `${value}` : `${prev}${value}`
+    );
+  };
+
+  const handleClickOperationButton = (operator: keyof typeof EOPERATIONS) => {
+    /**최종 입력된 값이 숫자인지 연산자인지 */
+    const isLastWordTypeNumber = !isNaN(
+      +inputtedTotalValue[inputtedTotalValue.length - 1]
+    );
+    /**연산을 위한 숫자와 연산자들이 담긴 배열 ex)['1','+','2'] */
+    const arrItemsForCalc = inputtedTotalValue
+      .split(OPERATION_REGEX)
+      .filter(Boolean);
+
+    /**최종 입력된 값이 연산자인 상태에서 숫자가 아닌 연산자를 입력받았을 때  */
+    if (inputtedTotalValue === "0" || !isLastWordTypeNumber) {
+      alert("숫자를 먼저 입력한 후 연산자를 입력해주세요!");
+      return;
+    }
+
+    /**식이 완성되지 않은 상태에서 =을 눌렀을때 */
+    if (!(arrItemsForCalc.length >= 3) && operator === "=") {
+      return;
+    }
+
+    currNumber.current = undefined;
+    const num1 = +arrItemsForCalc[0];
+    const oper = arrItemsForCalc[1] as keyof typeof EOPERATIONS;
+    const num2 = +arrItemsForCalc[2];
+
+    if (operator === "=" && arrItemsForCalc.length >= 3) {
+      const calculatedResult = calculateByOperator(
+        oper,
+        num1,
+        num2
+      )?.toString();
+
+      if (calculatedResult === "Infinity") {
+        setInputtedTotalValue("오류");
+        currNumber.current = "0";
+        return;
+      }
+
+      currNumber.current = calculatedResult;
+      setInputtedTotalValue(calculatedResult);
+    } else {
+      setInputtedTotalValue((prev) => prev + operator);
     }
   };
 
-  const checkNumberLength = (num: number) => {
-    setCurrNumber((prev) => {
-      if (prev.toString().length === 3) {
-        alert("숫자는 세 자리까지만 입력 가능합니다!");
-        return prev;
-      } else {
-        return !!prev ? +`${prev}${num}` : num;
-      }
-    });
+  const handleClickAllClear = () => {
+    currNumber.current = undefined;
+    setInputtedTotalValue("0");
   };
+
   return (
     <>
       <div id="app">
         <div className="calculator">
-          <h1 id="total">{currNumber}</h1>
+          <h1 id="total">{inputtedTotalValue}</h1>
           <div className="digits flex">
             {[...CALCULATOR_NUMBERS].reverse().map((number, i) => (
               <NumberButton
                 label={number}
                 key={i}
-                onClick={handleClickButton}
+                onClick={handleClickNumberButton}
               />
             ))}
           </div>
           <div className="modifiers subgrid">
-            <button className="modifier">AC</button>
+            <button className="modifier" onClick={handleClickAllClear}>
+              AC
+            </button>
           </div>
           <div className="operations subgrid">
             {Object.values(EOPERATIONS).map((operation, i) => (
               <OperationButton
                 label={operation}
                 key={i}
-                onClick={handleClickButton}
+                onClick={handleClickOperationButton}
               />
             ))}
           </div>
---
+++ src/constants/index.ts
@@ -0,0 +1,1 @@
+export const OPERATION_REGEX = /([/+\-X=])/g;
