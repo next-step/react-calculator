6952a7bb4509ddf05688f6c0bef514a4681cda45
Leeyounwoo <yunwoo.lee@cj.net>
2024-02-26 09:24:27 +0900

    함수 역할 분리

--- src/components/DigitButton.jsx
+++ src/components/DigitButton.jsx
@@ -1,11 +1,16 @@
 /* eslint-disable react/prop-types */
 export default function DigitButton({ value, firstNumber, setFirstNumber, operationType, secondNumber, setSecondNumber }) {
-    const getValidateNumber = (prevNumber, nowClickNumber) => {
+    // 입력한 숫자의 범위 초과하는지 검증
+    const isMaxLengthExceeded = (prevNumber) => {
         if (prevNumber.length >= 3) {
             alert('숫자는 세 자리까지만 입력 가능합니다!');
-            return prevNumber;
+            return true;
         }
+        return false;
+    };
 
+    // 새로운 입력값 추가한 새로운 값 리턴
+    const getExtendedValue = (prevNumber, nowClickNumber) => {
         return String(Number(prevNumber + nowClickNumber));
     };
 
@@ -16,9 +21,26 @@ export default function DigitButton({ value, firstNumber, setFirstNumber, operat
                 alert('AC 버튼을 눌러주세요.');
                 return;
             }
-            setFirstNumber(getValidateNumber(firstNumber, nowClickValue));
+
+            // 최대 입력 범위 초과
+            if (isMaxLengthExceeded(firstNumber)) {
+                setFirstNumber(String(firstNumber));
+                return;
+            }
+
+            // 연장된 값
+            const extendedValue = getExtendedValue(firstNumber, nowClickValue);
+            setFirstNumber(extendedValue);
         } else {
-            setSecondNumber(getValidateNumber(secondNumber, nowClickValue));
+            // 최대 입력 범위 초과
+            if (isMaxLengthExceeded(secondNumber)) {
+                setSecondNumber(String(secondNumber));
+                return;
+            }
+
+            // 연장된 값
+            const extendedValue = getExtendedValue(secondNumber, nowClickValue);
+            setSecondNumber(extendedValue);
         }
     };
 
