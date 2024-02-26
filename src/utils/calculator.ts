215457c12edf27f22cd9afeb68d21e963301bb5d
jonghyeon <jonghyeonLee.90@gmail.com>
2024-02-26 09:09:31 +0900

    코드정리

--- src/utils/calculator.ts
+++ src/utils/calculator.ts
@@ -1,18 +1,18 @@
 // 계산기 클래스
 export class Calculator {
-    readonly num1: number = 0;
-    readonly num2: number = 0;
+  readonly num1: number = 0;
+  readonly num2: number = 0;
 
-    constructor( num1:number , num2: number) {
-        this.num1 = num1;
-        this.num2 = num2;
-    }
+  constructor(num1: number, num2: number) {
+    this.num1 = num1;
+    this.num2 = num2;
+  }
 
-    add = () => this.num1 + this.num2
+  add = () => this.num1 + this.num2;
 
-    minus = () => this.num1 - this.num2
+  minus = () => this.num1 - this.num2;
 
-    multiple = () => this.num1 * this.num2
+  multiple = () => this.num1 * this.num2;
 
-    divide = () => Math.floor(this.num1 / this.num2)
+  divide = () => Math.floor(this.num1 / this.num2);
 }
