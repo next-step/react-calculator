00bdc7149f44fcbd981e02531ad12770ad873cb4
jonghyeon <jonghyeon90@com2us.com>
2024-03-11 11:25:32 +0900

    refactor: fragment 태그 제거

--- src/component/DisplayNumber.tsx
+++ src/component/DisplayNumber.tsx
@@ -1,9 +1,3 @@
-const DisplayNumber = ({ displayNumber }: { displayNumber: string }) => {
-  return (
-    <>
-      <h1 id="total">{displayNumber}</h1>
-    </>
-  );
-};
+const DisplayNumber = ({ displayNumber }: { displayNumber: string }) => <h1 id="total">{displayNumber}</h1>;
 
 export default DisplayNumber;
